db8a8dafd78b1d4f85d99cebea3aa08f6f002a72
Wang Shiyao <furytoar@gmail.com>
2020-02-07 19:11:13 +0800

    update: procTable, unit tests

--- Team00/Code00/source/PKB.h
+++ Team00/Code00/source/PKB.h
@@ -10,17 +10,35 @@ using namespace std;
 typedef short PROC;
 typedef string VarName;
 typedef int VarId;
-
+typedef int ProcId;
+typedef string ProcName;
 
 class TNode;
 
-// class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+class Procedure {
+public:
+	ProcName procName;
+	Procedure(ProcName procName) {
+		this->procName = procName;
+	}
+	std::wstring ToString(const Procedure& p) {
+		return L"adsfasd";
+	}
+	
+	// a way to get around unit testing AreEquals errors for object checking
+	bool equals(const Procedure& p) { 
+		return procName == p.procName;
+	}
+};
 
 class VarTable {
-	static unordered_map<VarName, VarId> nameIdTable;
-	static unordered_map<VarId, VarName> idNameTable;
-	static VarId varIdGenerator;
+	unordered_map<VarName, VarId> nameIdTable;
+	unordered_map<VarId, VarName> idNameTable;
+	VarId varIdGenerator;
 public:
+	VarTable() {
+		varIdGenerator = 0;
+	}
 
 	// If varName is not in the VarTable, varName is inserted into the VarTable. Else, the table remains unchanged.
 	// Returns the ID of the variable in the VarTable.
@@ -45,7 +63,7 @@ public:
 		try {
 			return nameIdTable.at(varName);
 		}
-		catch(const out_of_range &e) {
+		catch(const out_of_range& e) {
 			return -1;
 		}
 	}
@@ -58,8 +76,53 @@ public:
 
 class ProcTable {
 
-};
+	unordered_map<ProcName, ProcId> nameIdTable;
+	unordered_map<ProcId, Procedure> idProcTable;
+	ProcId procIdGenerator;
 
+public:
+
+	ProcTable() {
+		procIdGenerator = 0;
+	}
+
+	// If proc is not in the ProcTable, proc is inserted into the ProcTable. Else, the table remains unchanged.
+	// Returns the ID of the procedure in the ProcTable.
+	ProcId insertProc(Procedure proc) {
+		ProcName procName = proc.procName;
+		if (nameIdTable.try_emplace(procName, procIdGenerator).second) {
+			ProcId thisId = procIdGenerator;
+			procIdGenerator++;
+			idProcTable.insert(make_pair(thisId, proc));
+			return thisId;
+		}
+		else {
+			return getProcId(procName);
+		}
+	}
+
+	// Returns the procedure object at the given ID in the ProcTable. Throws an exception if the ID is not found in the table.
+	Procedure get(ProcId procId) {
+		return idProcTable.at(procId); // throws out_of_range exception
+	}
+
+	// Returns the ID of proc in the ProcTable. If proc is not found, -1 is returned.
+	ProcId getProcId(ProcName procName) {
+		try {
+			return nameIdTable.at(procName);
+		}
+		catch (const out_of_range & e) {
+			cout << e.what();
+			return -1;
+		}
+	}
+
+	// Returns the number of procedures in the ProcTable.
+	int size() {
+		return nameIdTable.size();
+	}
+
+};
 class PKB {
 public:
 	static VarTable* varTable; 
