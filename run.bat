a6a484af7bdd88b1a4fe8d567335fd7903e54087
zj-cs2103 <zj.cs2103@gmail.com>
2020-02-09 04:53:46 +0800

    travis ci setup test

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-cmd.exe /c ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" "Team06/Code06/StartupSPASolution.sln""
+cmd.exe /c ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" "Team06/Code06/StartupSPASolution.sln" "/p:PlatformToolset=v141""
