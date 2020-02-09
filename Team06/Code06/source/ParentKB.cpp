3383fe64a99b90fdf155ba789b302a06e0ac7717
laichengyu <laichengyu95@gmail.com>
2020-02-10 02:26:18 +0800

    Redesign implementation of Parent(*) API

--- Team06/Code06/source/ParentKB.cpp
+++ Team06/Code06/source/ParentKB.cpp
@@ -1,28 +1,72 @@
 #include "ParentKB.h"
 
-ParentKB::ParentKB(int numProgStmts)
-	: numProgStmts(numProgStmts)
+/*
+	TODO: Exception handling for invalid inputs
+*/
+
+ParentKB::ParentKB()
 {
-	parentTable = std::vector<std::vector<bool>>(numProgStmts, std::vector<bool>(numProgStmts));
-	parentStarTable = std::vector<std::vector<bool>>(numProgStmts, std::vector<bool>(numProgStmts));
+
 }
 
+/*
+	TODO: Move add Follows* logic out to Design Extractor
+*/
 void ParentKB::addParent(int stmtId1, int stmtId2)
 {
-	parentTable[stmtId1][stmtId2] = true;
-}
+	if (parentTable.find(stmtId1) == parentTable.end())
+	{
+		parentTable.insert(std::make_pair(stmtId1, parentRS()));
+	}
 
-void ParentKB::addParentStar(int stmtId1, int stmtId2)
-{
-	parentStarTable[stmtId1][stmtId2] = true;
+	if (parentTable.find(stmtId2) == parentTable.end())
+	{
+		parentTable.insert(std::make_pair(stmtId2, parentRS()));
+	}
+
+	parentRS pRS1 = parentTable[stmtId1];
+	parentRS pRS2 = parentTable[stmtId2];
+
+	pRS1.child = stmtId2;
+	pRS2.parent = stmtId1;
+
+	for (auto parent : pRS1.allParents)
+	{
+		parentTable[parent].allChildren.insert(stmtId2);
+	}
+
+	pRS2.allParents = pRS1.allParents;
+	pRS2.allParents.insert(stmtId1);
 }
 
 bool ParentKB::parent(int stmtId1, int stmtId2)
 {
-	return parentTable[stmtId1][stmtId2];
+	return parentTable[stmtId1].child == stmtId2;
 }
 
 bool ParentKB::parentStar(int stmtId1, int stmtId2)
 {
-	return parentStarTable[stmtId1][stmtId2];
+	parentRS pRS1 = parentTable[stmtId1];
+	std::unordered_set<int> parent1Children = pRS1.allChildren;
+	return parent1Children.find(stmtId2) != parent1Children.end();
+}
+
+int ParentKB::getParent(int stmtId)
+{
+	return parentTable[stmtId].parent;
+}
+
+int ParentKB::getChild(int stmtId)
+{
+	return parentTable[stmtId].child;
+}
+
+std::unordered_set<int> ParentKB::getAllParents(int stmtId)
+{
+	return parentTable[stmtId].allParents;
+}
+
+std::unordered_set<int> ParentKB::getAllChildren(int stmtId)
+{
+	return parentTable[stmtId].allChildren;
 }
--- Team06/Code06/source/ParentKB.h
+++ Team06/Code06/source/ParentKB.h
@@ -1,22 +1,58 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+struct parentRS
+{
+	int parent = 0;
+	int child = 0;
+	std::unordered_set<int> allParents;
+	std::unordered_set<int> allChildren;
+};
 
 class ParentKB
 {
 public:
-	ParentKB(int numProgStmts);
+	ParentKB();
 
+	/*
+		Adds Parent(stmtId1, stmtId2) relation to parentTable.
+		Also adds Parent*(s, stmtId2) for all s in s1.allParents.
+	*/
 	void addParent(int stmtId1, int stmtId2);
 
-	void addParentStar(int stmtId1, int stmtId2);
-
+	/*
+		Returns TRUE if Parent(stmtId1, stmtId2) is true, FALSE otherwise.
+	*/
 	bool parent(int stmtId1, int stmtId2);
 
+	/*
+		Returns TRUE if Parent*(stmtId1, stmtId2) is true, FALSE otherwise.
+	*/
 	bool parentStar(int stmtId1, int stmtId2);
 
+	/*
+		Returns statement ID s for which Parent(s, stmtId) is true.
+	*/
+	int getParent(int stmtId);
+
+	/*
+		Returns statement ID s for which Parent(stmtId, s) is true.
+	*/
+	int getChild(int stmtId);
+
+	/*
+		Returns all statement IDs s for which Parent*(s, stmtId) is true.
+	*/
+	std::unordered_set<int> getAllParents(int stmtId);
+
+	/*
+		Returns all statement IDs s for which Parent*(stmtId, s) is true.
+	*/
+	std::unordered_set<int> getAllChildren(int stmtId);
+
 private:
-	const int numProgStmts;
-	static std::vector<std::vector<bool>> parentTable;
-	static std::vector<std::vector<bool>> parentStarTable;
+	static std::unordered_map<int, parentRS> parentTable;
 };
