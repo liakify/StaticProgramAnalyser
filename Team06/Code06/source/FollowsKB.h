194601263653954f83243952cb12ae8fabb12128
laichengyu <laichengyu95@gmail.com>
2020-02-09 20:52:26 +0800

    Add comments for Follows API

--- Team06/Code06/source/FollowsKB.h
+++ Team06/Code06/source/FollowsKB.h
@@ -7,12 +7,24 @@ class FollowsKB
 public:
 	FollowsKB(int numProgStmts);
 
+	/*
+		Adds Follows(stmtId1, stmtId2) relation to followsTable.
+	*/
 	void addFollows(int stmtId1, int stmtId2);
 
+	/*
+		Adds Follows*(stmtId1, stmtId2) relation to followStarTable.
+	*/
 	void addFollowStar(int stmtId1, int stmtId2);
 
+	/*
+		Returns TRUE if Follows(stmtId1, stmtId2) is true, FALSE otherwise.
+	*/
 	bool follows(int stmtId1, int stmtId2);
 
+	/*
+		Returns TRUE if Follows*(stmtId1, stmtId2) is true, FALSE otherwise.
+	*/
 	bool followStar(int stmtId1, int stmtId2);
 
 private:
