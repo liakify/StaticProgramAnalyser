b1d35c187893a580d964ba5dcc1771dd942e3426
laichengyu <laichengyu95@gmail.com>
2020-02-10 02:08:20 +0800

    Redesign implementation for Follows(*) API

--- Team06/Code06/source/FollowsKB.cpp
+++ Team06/Code06/source/FollowsKB.cpp
@@ -1,28 +1,72 @@
 #include "FollowsKB.h"
 
-FollowsKB::FollowsKB(int numProgStmts)
-	: numProgStmts(numProgStmts)
+/*
+	TODO: Exception handling for invalid inputs
+*/
+
+FollowsKB::FollowsKB()
 {
-	followsTable = std::vector<std::vector<bool>>(numProgStmts, std::vector<bool>(numProgStmts));
-	followStarTable = std::vector<std::vector<bool>>(numProgStmts, std::vector<bool>(numProgStmts));
+
 }
 
+/*
+	TODO: Move add Follows* logic out to Design Extractor
+*/
 void FollowsKB::addFollows(int stmtId1, int stmtId2)
 {
-	followsTable[stmtId1][stmtId2] = true;
-}
+	if (followsTable.find(stmtId1) == followsTable.end())
+	{
+		followsTable.insert(std::make_pair(stmtId1, followsRS()));
+	}
+	
+	if (followsTable.find(stmtId2) == followsTable.end())
+	{
+		followsTable.insert(std::make_pair(stmtId2, followsRS()));
+	}
 
-void FollowsKB::addFollowStar(int stmtId1, int stmtId2)
-{
-	followStarTable[stmtId1][stmtId2] = true;
+	followsRS fRS1 = followsTable[stmtId1];
+	followsRS fRS2 = followsTable[stmtId2];
+
+	fRS1.follower = stmtId2;
+	fRS2.following = stmtId1;
+
+	for (auto following : fRS1.allFollowing)
+	{
+		followsTable[following].allFollowers.insert(stmtId2);
+	}
+
+	fRS2.allFollowing = fRS1.allFollowing;
+	fRS2.allFollowing.insert(stmtId1);
 }
 
 bool FollowsKB::follows(int stmtId1, int stmtId2)
 {
-	return followsTable[stmtId1][stmtId2];
+	return followsTable[stmtId1].follower == stmtId2;
 }
 
 bool FollowsKB::followStar(int stmtId1, int stmtId2)
 {
-	return followStarTable[stmtId1][stmtId2];
+	followsRS fRS1 = followsTable[stmtId1];
+	std::unordered_set<int> stmt1Followers = fRS1.allFollowers;
+	return stmt1Followers.find(stmtId2) != stmt1Followers.end();
+}
+
+int FollowsKB::getFollower(int stmtId)
+{
+	return followsTable[stmtId].follower;
+}
+
+int FollowsKB::getFollowing(int stmtId)
+{
+	return followsTable[stmtId].following;
+}
+
+std::unordered_set<int> FollowsKB::getAllFollowers(int stmtId)
+{
+	return followsTable[stmtId].allFollowers;
+}
+
+std::unordered_set<int> FollowsKB::getAllFollowing(int stmtId)
+{
+	return followsTable[stmtId].allFollowing;
 }
--- Team06/Code06/source/FollowsKB.h
+++ Team06/Code06/source/FollowsKB.h
@@ -1,21 +1,27 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+struct followsRS
+{
+	int following = 0;
+	int follower = 0;
+	std::unordered_set<int> allFollowing;
+	std::unordered_set<int> allFollowers;
+};
 
 class FollowsKB 
 {
 public:
-	FollowsKB(int numProgStmts);
+	FollowsKB();
 
 	/*
 		Adds Follows(stmtId1, stmtId2) relation to followsTable.
+		Also adds Follows*(s, stmtId2) for all s in s1.allFollowed.
 	*/
 	void addFollows(int stmtId1, int stmtId2);
-
-	/*
-		Adds Follows*(stmtId1, stmtId2) relation to followStarTable.
-	*/
-	void addFollowStar(int stmtId1, int stmtId2);
 
 	/*
 		Returns TRUE if Follows(stmtId1, stmtId2) is true, FALSE otherwise.
@@ -27,8 +33,26 @@ public:
 	*/
 	bool followStar(int stmtId1, int stmtId2);
 
+	/*
+		Returns statement ID s for which Follow(stmtId, s) is true.
+	*/
+	int getFollower(int stmtId);
+
+	/*
+		Returns statement ID s for which Follow(s, stmtId) is true.
+	*/
+	int getFollowing(int stmtId);
+
+	/*
+		Returns all statement IDs s for which Follows*(stmtId, s) is true.
+	*/
+	std::unordered_set<int> getAllFollowers(int stmtId);
+
+	/*
+		Returns all statement IDs s for which Follows*(s, stmtId) is true.
+	*/
+	std::unordered_set<int> getAllFollowing(int stmtId);
+
 private:
-	const int numProgStmts;
-	static std::vector<std::vector<bool>> followsTable;
-	static std::vector<std::vector<bool>> followStarTable;
+	static std::unordered_map<int, followsRS> followsTable;
 };
