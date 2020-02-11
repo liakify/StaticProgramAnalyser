1560edc0b2837d9285be0df6d575707b27f1cf0f
zj-cs2103 <46818381+zj-cs2103@users.noreply.github.com>
2020-02-12 00:37:26 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,5 +5,5 @@ wget https://infocommsociety.com/~zhijian/cs3203/cppunittest.zip
 7z x cppunittest.zip
 dir
 set "INCLUDE=C:\Users\travis\build\nus-cs3203\team06-win-spa-19s2\CppUnitTest\include\;%INCLUDE%"
-set "LIB=C:\Users\travis\build\nus-cs3203\team06-win-spa-19s2\CppUnitTest\lib\x86\;%LIB%"
+set "LIB=C:\Users\travis\build\nus-cs3203\team06-win-spa-19s2\CppUnitTest\lib\;%LIB%"
 cmd.exe /c ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" "Team06/Code06/StartupSPASolution.sln" "/p:PlatformToolset=v141" "/p:UseEnv=true" "/p:Configuration=Debug" "/p:Platform=Win32""
