fcb30b5751c9dd7a113c0986b157208e903d5ad0
zj-cs2103 <46818381+zj-cs2103@users.noreply.github.com>
2020-02-12 00:04:01 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
 wget https://infocommsociety.com/~zhijian/cs3203/cppunittest.zip
 7z x cppunittest.zip
 dir
