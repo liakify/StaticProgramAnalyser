f7ce3bda3c1562fa7d21bb68fe660430b290944e
lormee94 <lormee94@github.com>
2020-02-20 02:32:42 +0800

    Fix formattedResult.unique() - That method doesn't work - Changed to inserting values into set to remove duplicates before transferring to list.

--- Team06/Code06/source/QueryProjector.cpp
+++ Team06/Code06/source/QueryProjector.cpp
@@ -1,4 +1,5 @@
 #include "QueryProjector.h"
+#include <set>
 #include <sstream>
 
 namespace PQL {
@@ -8,18 +9,22 @@ namespace PQL {
     }
     
     std::list<std::string> QueryProjector::getFormattedResult() {
-        std::list<std::string> formattedResult;
+        //Use set to remove duplicates
+        std::set<std::string> uniqueResultSet;
 
         for (auto itr1 = this->result.begin(); itr1 != this->result.end(); ++itr1) {
             ClauseResultEntry clauseResultEntry = *itr1;
             for (auto itr2 = clauseResultEntry.begin(); itr2 != clauseResultEntry.end(); ++itr2) {
-              std::stringstream s;
-              s << itr2->second;
-              formattedResult.push_back(s.str());
+                std::stringstream s;
+                s << itr2->second;
+                uniqueResultSet.insert(s.str());
             }
         }
 
-        formattedResult.unique();
+        std::list<std::string> formattedResult;
+        for (auto itr3 = uniqueResultSet.begin(); itr3 != uniqueResultSet.end(); ++itr3){
+            formattedResult.push_back(*itr3);
+        }
         return formattedResult;
     }
 }
