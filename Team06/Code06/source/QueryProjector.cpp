da7fb363a7297926511230739f2fa93331fcd3a7
lormee94 <lormee94@github.com>
2020-02-19 15:10:01 +0800

    QueryProjector basic implementation

--- Team06/Code06/source/QueryProjector.cpp
+++ Team06/Code06/source/QueryProjector.cpp
@@ -1,9 +1,23 @@
 #include "QueryProjector.h"
+#include <string>
 
 namespace PQL {
 
-    QueryProjector::QueryProjector() {
-        // TODO: Initialise query projector here
+    QueryProjector::QueryProjector(ClauseResult &result) {
+        this->result = result;
     }
     
+    std::list<std::string> QueryProjector::getFormattedResult() {
+        std::list<std::string> formattedResult;
+
+        for (auto itr1 = this->result.begin();
+              itr1 != this->result.end(); ++itr1) {
+            for (auto itr2 =*itr1.begin(); itr2 != *itr1.end(); ++ itr2) {
+              formattedResult.push_back(std::to_string(*itr2->second));
+            }
+        }
+
+        formattedResult.unique();
+        return formattedResult;
+    }
 }
--- Team06/Code06/source/QueryProjector.h
+++ Team06/Code06/source/QueryProjector.h
@@ -1,12 +1,33 @@
 #pragma once
 
-#include "PQLTypes.h"
+#include "Types.h"
+#include "PQL.h"
 
 namespace PQL {
 
+    /**
+     *  The QueryProjector takes the results from the QueryEvaluator
+     *  and formats it into the desired output by the SPA. In this case,
+     *  std::list<std::string>
+     */
     class QueryProjector {
     public:
-        QueryProjector();
+        /**
+          *  Constructor for QueryProjector class to format result from QueryResult
+          *
+          *  @param      result    ClauseResult returned by QueryEvaluator.
+          */
+        QueryProjector(ClauseResult &result);
+
+        /**
+          *  Constructor for QueryProjector class to format result from QueryResult
+          *
+          *  @return      List of strings containing query result.
+          */
+        std::list<std::string> getFormattedResult();
+
+    private:
+        ClauseResult result;
     };
 
 }
