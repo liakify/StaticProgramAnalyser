de68291024acfbd2fa6febc8c251623f327238ab
Aulud <44989315+Aulud@users.noreply.github.com>
2020-02-21 12:38:17 +0800

    Add full unit test suite for QueryUtils class

---
+++ Team06/Code06/UnitTesting/testQueryUtils.cpp
@@ -0,0 +1,188 @@
+#include "stdafx.h"
+#include "QueryUtils.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace PQL;
+
+namespace UnitTesting {
+	TEST_CLASS(TestQueryUtils) {
+		public:
+			string EMPTY_STRING = "";
+
+			vector<string> VALID_INTEGERS = { "0", "3203", "00000000" };
+			vector<string> INVALID_INTEGERS = { EMPTY_STRING, "-1", "3216 ", " 3217", "20 40", "0xF" };
+
+			vector<string> VALID_IDENTIFIERS = { "A", "fOr", "softwareEng1n33r1ng", "CS3203" };
+			vector<string> INVALID_IDENTIFIERS = { EMPTY_STRING, "1", "3203S", " Give", "us ", "A+", "thank you", "xoxo_team6" };
+
+			vector<string> VALID_STMT_REFS = { "_" , "3281", "dAmYtH" };
+			vector<string> INVALID_STMT_REFS = { EMPTY_STRING, "-65536", "2040S" };
+
+			vector<string> VALID_ENTITY_REFS = { "_", "synonym", "\"quotes\"", "\"  varQuotes \"" };
+			vector<string> INVALID_ENTITY_REFS = { EMPTY_STRING, "1", "1231S", " space ", "\"noEnd", "noStart\"", "\"\"dupes\"\"", "\"expr + 1\"" };
+
+			vector<string> VALID_PATTERNS = {
+				// Wildcard pattern
+				"_",
+				// Exact patterns with well-formed expressions
+				"\"nCOV19\"", "\"big % nUiSaNcE\"", "\"S + U - this * Mod / plox % thanks\"", "\"b - B + bB * Bb / b9 \"",
+				// Inclusive patterns with well-formed expressions
+				"_\"SARSagain\"_", "_\"deaths + 1\"_", "_\"big / KEK * 420\"_",
+				// Exact patterns with variably spaced expressions
+				// "  \"frontSpace\"", "\"backSpace\" ", "  \"both\" ", "\"420+69\"", "\" starts+here  \"", "\"fib/1 + 1*   two  - Thr33   \"",
+				// Inclusive patterns with variably spaced expressions
+				// " _\"var\"_  ", "_  \"b0th / S1des\" _", " _\"asymmetr1C \" _", " _ \" 3230 %   D *c - b+a\"_"
+			};
+			vector<string> INVALID_PATTERNS = {
+				// Missing or unmatched quotation marks
+				EMPTY_STRING, "3 + 5 - 7", "_b * b - 4 * A * c", "\"noClosing % quotation", "_missing * start\"_"
+				// Quoted wildcards
+				"\"_\"", "_\"_\"_",
+				// Invalid identifiers
+				"\"2good4me\"", "_\"1WO / eNc1k\"_", "\"pascal_case - sucks\"", "\"github.com\"", "_\"wat?\"_", "\"split\n - line\"",
+				// Unrecognised operators
+				"\"2 ^ 32\"", "_\"x ** x\"_", "\"x < 3\"", "_\"now == deadline\"_", "\"beep - (boop + bop)\"", "_\" n %= 1000000007\"_", "\"1 && 0\"",
+				// Invalid arithmetic expressions
+				"_\"A +\"_", "\"- 31415\"", "\"i++\"", "_\"20 20\"_", "\"taiWan / numba * 1 +\""
+				// Misplaced quotes or inclusive pattern underscores
+				"\"_inside_\"", "outside + \"inside\"", "\"looks\" + _\"cl0se\"_"
+				// Unmatched inclusive pattern underscores
+				"_\"missingRight\"", "\"missingLeft\"_", "  _ \"1\" ", "\"unmatchedHalf \"_"
+				// Supernumary inclusive pattern underscores
+				"_\"x + y\"__", "_ _\"x86ISA\"_", "_ _\"ARM\"_  _"
+			};
+
+			vector<pair<string, string>> LEFT_TRIM_PAIRS = {
+				{ EMPTY_STRING, EMPTY_STRING },
+				{ "var", "var" },
+				{ " left", "left" },
+				{ "      many_Left", "many_Left" },
+				{ "right ", "right " },
+				{ " multiple words", "multiple words" },
+				{ "\n\rlineBreaks", "lineBreaks" },
+				{ "\r left & right \n", "left & right \n" }
+			};
+
+			vector<pair<string, string>> RIGHT_TRIM_PAIRS = {
+				{ EMPTY_STRING, EMPTY_STRING },
+				{ "test", "test" },
+				{ "right ", "right" },
+				{ "many.Right    ", "many.Right" },
+				{ " left ", " left" },
+				{ "split tokens  ", "split tokens" },
+				{ "lineBreaks\n\r", "lineBreaks" },
+				{ "\r left and right \n", "\r left and right" }
+			};
+
+			vector<pair<string, string>> TRIMMED_STRING_PAIRS = {
+				{ EMPTY_STRING, EMPTY_STRING },
+				{ "idempot3nt", "idempot3nt" },
+				{ "\n \rfront & bacc _ g0n3  \r \n", "front & bacc _ g0n3" },
+				{ "< div&= >\n", "< div&= >" }
+			};
+
+			vector<pair<string, string>> STRIPPED_PATTERN_PAIRS = {
+				{ EMPTY_STRING, EMPTY_STRING },
+				{ "\"x\"", "x" },
+				{ "_\"DORSCON - orang3\"_", "_DORSCON-orang3_" },
+				{ "\" pls * 1  -star+rep0\"    ", "pls*1-star+rep0" },
+				{ " _\"n0ne+ l3ft *righT % both -  many  \" _","_n0ne+l3ft*righT%both-many_" },
+				{ "_  \"      wutDis  -1\" _  " , "_wutDis-1_" }
+			};
+
+			vector<pair<pair<string, char>, pair<string, string>>> SPLIT_STRING_PAIRS = {
+				{ { EMPTY_STRING, ' ' }, { EMPTY_STRING, EMPTY_STRING } },
+				{ { "a b ", ' ' }, { "a", "b" } },
+				{ { "a b c", ' ' }, { "a", "b c" } },
+				{ { "  p q l ", ' ' }, { "", "p q l" } },
+				{ { " x ,y, z  ", ',' }, { "x", "y, z" } },
+				{ { " p k b ", '.' }, { "p k b", EMPTY_STRING } },
+				{ { "std::out << var;", ':' }, { "std", ":out << var;" } },
+				{ { " <div> text </div>  ", '>' }, { "<div", "text </div>" } },
+				{ { "x++; y--; z += y ; ", ';' }, { "x++", "y--; z += y ;" } },
+				{ { "Modifies ( stt, var) ", '(' }, { "Modifies", "stt, var)" } },
+				{ { "Select < x, BOOLEAN, var > ", '<' }, { "Select", "x, BOOLEAN, var >" } }
+			};
+			
+			vector<pair<pair<string, char>, vector<string>>> TOKENISED_STRING_PAIRS = {
+				{ { EMPTY_STRING, ' ' }, { } },
+				{ { "no_space", ' ' }, { "no_space" } },
+				{ { "  www.google.com ", '/' }, { "www.google.com" } },
+				{ { "a la carbonara", 'a' }, { "l", "c", "rbon", "r" } },
+				{ { " t z  e r   b i n ", ' ' }, { "t", "z", "e", "r", "b", "i", "n" } },
+				{ { "<p> <b> sample HTML </b> </p>", '>' }, { "<p", "<b", "sample HTML </b", " </p" } },
+				{ { " <tuple,p, q , r > ", ',' }, { "<tuple", "p", "q", "r >" } },
+				{ { "pattern ( Uses ,_, _) ", ',' }, { "pattern ( Uses", "_", "_)" } },
+				{ { " assign a1", ',' }, { "assign a1" } },
+				{ { " stmt s ,s1, s2, s3 ", ',' }, { "stmt s", "s1", "s2", "s3" } },
+				{ { "stmt s1,s2 ;call c; ifs  i1,   i2 ;  while w  ; Select s1", ';' }, { "stmt s1,s2", "call c", "ifs  i1,   i2", "while w", "Select s1" } }
+			};
+
+			TEST_METHOD(TestIsInteger) {
+				for (auto input : VALID_INTEGERS) {
+					Assert::IsTrue(QueryUtils::isInteger(input));
+				}
+				for (auto input : INVALID_INTEGERS) {
+					Assert::IsFalse(QueryUtils::isInteger(input));
+				}
+			}
+
+			TEST_METHOD(TestIsValidIdentifier) {
+				for (auto input : VALID_IDENTIFIERS) {
+					Assert::IsTrue(QueryUtils::isValidIdentifier(input));
+				}
+				for (auto input : INVALID_IDENTIFIERS) {
+					Assert::IsFalse(QueryUtils::isValidIdentifier(input));
+				}
+			}
+
+			TEST_METHOD(TestIsValidStmtRef) {
+				for (auto input : VALID_STMT_REFS) {
+					Assert::IsTrue(QueryUtils::isValidStmtRef(input));
+				}
+				for (auto input : INVALID_STMT_REFS) {
+					Assert::IsFalse(QueryUtils::isValidStmtRef(input));
+				}
+			}
+
+			TEST_METHOD(TestIsValidPattern) {
+				for (auto input : VALID_PATTERNS) {
+					Assert::IsTrue(QueryUtils::isValidPattern(input));
+				}
+				for (auto input : INVALID_PATTERNS) {
+					Assert::IsFalse(QueryUtils::isValidPattern(input));
+				}
+			}
+
+			TEST_METHOD(TestTrimMethods) {
+				for (auto testcase : LEFT_TRIM_PAIRS) {
+					Assert::IsTrue(QueryUtils::leftTrim(testcase.first) == testcase.second);
+				}
+				for (auto testcase : RIGHT_TRIM_PAIRS) {
+					Assert::IsTrue(QueryUtils::rightTrim(testcase.first) == testcase.second);
+				}
+				for (auto testcase : TRIMMED_STRING_PAIRS) {
+					Assert::IsTrue(QueryUtils::trimString(testcase.first) == testcase.second);
+				}
+			}
+
+			TEST_METHOD(TestStripPattern) {
+				for (auto testcase : STRIPPED_PATTERN_PAIRS) {
+					Assert::IsTrue(QueryUtils::stripPattern(testcase.first) == testcase.second);
+				}
+			}
+
+			TEST_METHOD(TestSplitString) {
+				for (auto testcase : SPLIT_STRING_PAIRS) {
+					Assert::IsTrue(QueryUtils::splitString(testcase.first.first, testcase.first.second) == testcase.second);
+				}
+			}
+
+			TEST_METHOD(TestTokeniseString) {
+				for (auto testcase : SPLIT_STRING_PAIRS) {
+					Assert::IsTrue(QueryUtils::splitString(testcase.first.first, testcase.first.second) == testcase.second);
+				}
+			}
+
+	};
+}
