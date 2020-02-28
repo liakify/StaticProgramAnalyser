6985e498357f5273f4059cdf657f120370c92218
Wang Shiyao <furytoar@gmail.com>
2020-02-28 23:34:07 +0800

    test: update pattern queries

--- Team06/Tests06/Scratch.py
+++ Team06/Tests06/Scratch.py
@@ -1,5 +1,57 @@
-answer = ""
-for x in range(34, 49):
-  answer += str(x) + ", "
+sel = "Select "
+suchthat = 'such that '
+pat = "Pattern"
 
-print(answer)
+comment_invalid_args = " - invalid: argument type"
+
+suchthatclauses = ['Follows ', 'Follows*', 'Parent ', 'Parent*']
+
+designEntities = {"stmt": 's', 
+  "read" : 'r',
+ "print" : 'pn',
+ "call": 'c',
+  "while" : 'w',
+  "if": 'ifs',
+"assign" : 'a',
+ "variable": 'v',
+  "constant": 'cn',
+  "procedure" : 'p',
+   "prog_line": 'pl'
+   }
+
+relRef = ["Follows", "Follows*", "Parent", "Parent*",
+    "Modifies", "Uses", "Calls", "Calls*",
+    "Next", "NextT", "Affects", "Affects*" ] 
+
+attrNames = ["stmt#", "procName", "varName", "value"]
+
+for de in designEntities:
+  # query = comment_invalid_args + '\n'
+  # query += de + ' ' + designEntities[de] + ';' + '\n'
+  # query += sel + designEntities[de] + ' ' + suchthat + 'Calls(' + designEntities[de] + ', _)' + '\n'
+  # query += "\n"
+  # query += "5000" + '\n'
+  # query += comment_invalid_args + '\n'
+  # query += de + ' ' + designEntities[de] + ';' + '\n'
+  # query += sel + designEntities[de] + ' ' + suchthat + 'Calls(_, ' + designEntities[de] + ')' + '\n'
+  # query += "\n"
+  # query += "5000"
+
+  query = comment_invalid_args + '\n'
+  query += de + ' ' + designEntities[de] + '; assign a;' + '\n'
+  query += sel + designEntities[de] + ' ' + suchthat + 'Pattern a ' + '('+ designEntities[de] + ', _)' + '\n'
+  query += "\n"
+  query += "5000" + '\n'
+  query += comment_invalid_args + '\n'
+  query += de + ' ' + designEntities[de] + '; assign a;' + '\n'
+  query += sel + designEntities[de] + ' ' + suchthat + 'Pattern a ' + '(_, '+ designEntities[de] + ')' + '\n'
+  query += "\n"
+  query += "5000"
+  print(query)
+
+
+# answer = ""
+# for x in range(1, 13):
+#   answer += str(x) + ", "
+# print(answer)
+
