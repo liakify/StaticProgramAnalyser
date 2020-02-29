988c8728bb78d6c55953c9f7a5a9d467b7bc69b6
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-03-01 01:40:44 +0800

    Add check for PKB for FrontEnd parsing of source - Variables - Constants - Procedure (one) - Statements and various types

--- Team06/Code06/IntegrationTesting/test-FrontEnd_PKB.cpp
+++ Team06/Code06/IntegrationTesting/test-FrontEnd_PKB.cpp
@@ -11,7 +11,7 @@ namespace IntegrationTesting
   PKB::PKB pkb;
   FrontEnd::Parser parser;
 
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(Test-FrontEnd_PKB)
 	{
     std::string src = "procedure p{x = 7; y = 2; if(x>y) then {z = x + y;} else {print y;} read a; }";
 
@@ -21,7 +21,16 @@ namespace IntegrationTesting
     allVars.insert("z");
     allVars.insert("a");
 
+    std::list<std::string> allConstants = {"7", "2"};
 
+    std::string procedureName = "p";
+
+    std::int stmtCount_ASSIGN = 3;
+    std::int stmtCount_READ = 1;
+    std::int stmtCount_PRINT = 1;
+    std::int stmtCount_CALL = 0;
+    std::int stmtCount_IF = 1;
+    std::int stmtCount_WHILE = 0;
 
 	public:
 		TEST_CLASS_INITIALIZE(setup) {
@@ -29,14 +38,24 @@ namespace IntegrationTesting
       pkb = parser.parseSimple(src);
     }
 
-		TEST_METHOD(Test-FrontEnd_PKB)
+		TEST_METHOD(parseSimple)
 		{
 			CollectionAssert::AreEqual(pkb.varTable.getAllVars(), allVars);
-      Assert::IsTrue(pkb.constTable.size() == 2);
-     	Assert::IsFalse(pkb.constTable.getConstId("7") == -1);
-      Assert::IsFalse(pkb.constTable.getConstId("2") == -1);
+
+      Assert::IsTrue(pkb.constTable.size() == allConstants.size());
+      for (auto itr = allConstants.begin(); itr != allConstants.end(); ++itr){
+        Assert::IsFalse(pkb.constTable.getConstId(*itr) == -1);
+      }
+     	
       Assert::IsTrue(pkb.procTable.size() == 1);
-      Assert::IsFalse(pkb.constTable.getProcId("p") == -1);
+      Assert::IsFalse(pkb.constTable.getProcId(procedureName) == -1);
+
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::ASSIGN).size() == stmtCount_ASSIGN);
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::READ).size() == stmtCount_READ);
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::PRINT).size() == stmtCount_PRINT);
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::CALL).size() == stmtCount_CALL);
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::IF).size() == stmtCount_IF);
+      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::WHILE).size() == stmtCount_WHILE);
 		}
 
 	};
