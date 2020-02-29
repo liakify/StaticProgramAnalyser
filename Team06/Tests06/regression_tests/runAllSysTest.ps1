6a6299a14b1058a362cb870c554faec92cd70edc
Wang Shiyao <furytoar@gmail.com>
2020-02-29 16:38:20 +0800

    add pass/fail indicators

--- Team06/Tests06/regression_tests/runAllSysTest.ps1
+++ Team06/Tests06/regression_tests/runAllSysTest.ps1
@@ -5,7 +5,9 @@ $query_suffix = '_queries.txt'
 $output_suffix = '_out.xml'
 $regression_test_path = '.\Team06\Tests06\regression_tests\'
 $list_of_test_files = @(
-'select_0_1'
+  # SAMPLE FAILED TEST CASE (to verify that this works)
+  # '..\modifies_0',
+  'select_0_1',
 'follows_0',
 'followsStar_0',
 'parent_0',
@@ -17,29 +19,18 @@ $list_of_test_files = @(
 )
 
 Try {
-
   foreach ($test in $list_of_test_files) {
-    .\Team06\Code06\Debug\AutoTester.exe $regression_test_path$test$source_suffix $regression_test_path$test$query_suffix $regression_test_path$test$output_suffix
+    .\Team06\Code06\Debug\AutoTester.exe $regression_test_path$test$source_suffix $regression_test_path$test$query_suffix $regression_test_path$test$output_suffix *>$null
     [XML]$output = Get-Content $regression_test_path$test$output_suffix
     
     foreach ($query in $output.test_results.queries.query) {
       if ($null -eq $query.passed) {
-        "SYSTEM TEST FAILED: "
+        "SYSTEM TEST FAILED in: " + $test
         exit 1
       }
     }
+    'TEST CASES in ' + $test + ' PASSED'
   }
-  
-  # SAMPLE FAILED TEST CASE (to verify that this works)
-  # .\Team06\Code06\Debug\AutoTester.exe .\Team06\Tests06\modifies_0_source.txt .\Team06\Tests06\modifies_0_queries.txt .\Team06\Tests06\modifies_0_out.xml
-  # [XML]$output2 = Get-Content .\Team06\Tests06\modifies_0_out.xml
-  
-  # foreach ($query in $output2.test_results.queries) {
-  #   if ($null -eq $query.passed) {
-  #     "SYSTEM TEST FAILED: "
-  #     exit 1
-  #   }
-  # }
 }
 Catch {
   exit 1
