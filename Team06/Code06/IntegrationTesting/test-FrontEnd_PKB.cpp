6e15a1b68a612a0eba42e75ab0bd415080e739d3
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-03-03 19:18:34 +0800

    Fix errors - Compilation issue involving multiple declaration - Travis detected errors

--- Team06/Code06/IntegrationTesting/test-FrontEnd_PKB.cpp
+++ Team06/Code06/IntegrationTesting/test-FrontEnd_PKB.cpp
@@ -1,148 +1,159 @@
+#pragma once
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "Parser.h"
-#include "PKB.h"
+#include "FrontEnd.h"
 #include "Types.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace IntegrationTesting
-{		
-  PKB::PKB pkb;
-  FrontEnd::Parser parser;
+namespace IntegrationTesting {
 
-	TEST_CLASS(Test-FrontEnd_PKB)
-	{
-    std::string src = "procedure p{x=7; y=2; if(x>y) then {z=x+a;} else {print y;} read y; }";
+	PKB::PKB pkb;
+	FrontEnd::FrontEndManager frontEnd = FrontEnd::FrontEndManager();
 
-    std::unordered_set<VarName> allVars = std::unordered_set<VarName>({"x", "y", "z", "a"});
-
-    std::unordered_set<VarName> allModifiedVars = std::unordered_set<VarName>({"x", "y", "z"});
-
-    std::unordered_set<StmtId> allModifiedStmts = std::unordered_set<StmtId>({1, 2, 4});
-
-    std::unordered_set<VarName> allUsedVars = std::unordered_set<VarName>({"x", "y", "a"});
-
-    std::unordered_set<StmtId> allUsedStmts = std::unordered_set<StmtId>({4, 5});
-
-    std::list<std::string> allConstants = {"7", "2"};
-
-    std::string procedureName = "p";
-
-    std::int stmtListCount = 3;
-    std::int stmtCount_ASSIGN = 3;
-    std::int stmtCount_READ = 1;
-    std::int stmtCount_PRINT = 1;
-    std::int stmtCount_CALL = 0;
-    std::int stmtCount_IF = 1;
-    std::int stmtCount_WHILE = 0;
-
+	TEST_CLASS(TestFrontEnd_PKB) {
 	public:
-		TEST_CLASS_INITIALIZE(setup) {
-      parser = FrontEnd::Parser();
-      pkb = parser.parseSimple(src);
-    }
 
-		TEST_METHOD(parseSimple)
-		{
-			CollectionAssert::AreEqual(pkb.varTable.getAllVars(), allVars);
+	std::unordered_set<VarName> allVars = std::unordered_set<VarName>({ "x", "y", "z", "a" });
 
-      Assert::IsTrue(pkb.constTable.size() == allConstants.size());
-      for (auto itr = allConstants.begin(); itr != allConstants.end(); ++itr){
-        Assert::IsFalse(pkb.constTable.getConstId(*itr) == -1);
-      }
-     	
-      Assert::IsTrue(pkb.procTable.size() == 1);
-      Assert::IsFalse(pkb.constTable.getProcId(procedureName) == -1);
-      
-      Assert::IsTrue(pkb.stmtListTable.size() == stmtListCount);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::ASSIGN).size() == stmtCount_ASSIGN);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::READ).size() == stmtCount_READ);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::PRINT).size() == stmtCount_PRINT);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::CALL).size() == stmtCount_CALL);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::IF).size() == stmtCount_IF);
-      Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::WHILE).size() == stmtCount_WHILE);
+	std::unordered_set<VarName> allModifiedVars = std::unordered_set<VarName>({ "x", "y", "z" });
 
-      Assert::IsTrue(pkb.followsKB.follows(1, 2));
-			Assert::IsTrue(pkb.followsKB.follows(2, 3));
-      Assert::IsTrue(pkb.followsKB.follows(3, 6));
-      Assert::IsTrue(pkb.followsKB.followStar(1, 2));
-      Assert::IsTrue(pkb.followsKB.followStar(1, 3));
-      Assert::IsTrue(pkb.followsKB.followStar(1, 6));
-      Assert::IsTrue(pkb.followsKB.followStar(2, 3));
-      Assert::IsTrue(pkb.followsKB.followStar(2, 6));
-      Assert::IsTrue(pkb.followsKB.followStar(3, 6));
+	std::unordered_set<StmtId> allModifiedStmts = std::unordered_set<StmtId>({ 1, 2, 4 });
 
-      Assert::IsFalse(pkb.followsKB.follows(3, 4));
-      Assert::IsFalse(pkb.followsKB.follows(4, 5));
-      Assert::IsFalse(pkb.followsKB.follows(5, 6));
-      Assert::IsFalse(pkb.followsKB.followStar(1, 4));
-      Assert::IsFalse(pkb.followsKB.followStar(1, 5));
-      Assert::IsFalse(pkb.followsKB.followStar(2, 4));
-      Assert::IsFalse(pkb.followsKB.followStar(2, 5));
-      Assert::IsFalse(pkb.followsKB.followStar(3, 4));
-      Assert::IsFalse(pkb.followsKB.followStar(3, 5));
-      Assert::IsFalse(pkb.followsKB.followStar(4, 5));
-      Assert::IsFalse(pkb.followsKB.followStar(4, 6));
-      Assert::IsFalse(pkb.followsKB.followStar(5, 6));
+	std::unordered_set<VarName> allUsedVars = std::unordered_set<VarName>({ "x", "y", "a" });
 
-      Assert::IsTrue(pkb.parentKB.parent(3, 4));
-      Assert::IsTrue(pkb.parentKB.parent(3, 5));
-      Assert::IsTrue(pkb.parentKB.parentStar(3, 4));
-      Assert::IsTrue(pkb.parentKB.parentStar(3, 5));
+	std::unordered_set<StmtId> allUsedStmts = std::unordered_set<StmtId>({ 4, 5 });
 
-      Assert::IsFalse(pkb.parentKB.parent(1, 2));
-      Assert::IsFalse(pkb.parentKB.parent(2, 3));
-      Assert::IsFalse(pkb.parentKB.parent(5, 6));
-      Assert::IsFalse(pkb.parentKB.parentStar(1, 2));
-      Assert::IsFalse(pkb.parentKB.parentStar(1, 3));
-      Assert::IsFalse(pkb.parentKB.parentStar(1, 4));
-      Assert::IsFalse(pkb.parentKB.parentStar(1, 5));
-      Assert::IsFalse(pkb.parentKB.parentStar(1, 6));
-      Assert::IsFalse(pkb.parentKB.parentStar(2, 3));
-      Assert::IsFalse(pkb.parentKB.parentStar(2, 4));
-      Assert::IsFalse(pkb.parentKB.parentStar(2, 5));
-      Assert::IsFalse(pkb.parentKB.parentStar(2, 6));
-      Assert::IsFalse(pkb.parentKB.parentStar(3, 6));
-      Assert::IsFalse(pkb.parentKB.parentStar(4, 5));
-      Assert::IsFalse(pkb.parentKB.parentStar(4, 6));
-      Assert::IsFalse(pkb.parentKB.parentStar(5, 6));
+	std::unordered_set<ConstValue> allConstants = { "7", "2" };
 
+	std::string procedureName = "p";
 
-      std::unordered_set<VarId> modifiedVarIds = pkb.modifiesKB.getAllVarsModifiedByProc(pkb.constTable.getProcId(procedureName));
-      std::unordered_set<VarName> modifiedVarNames;
-      std::unordered_set<StmtId> modifiedStmtIds;
-      for (auto itr = modifiedVarIds.begin(); itr != modifiedVarIds.end(); itr++){
-        modifiedVarNames.insert(pkb.varTable.get(*itr));
-        std::unordered_set<StmtId> modifiedStmts = pkb.modifiesKB.getAllStmtsModifyVar(*itr);
-        for (auto itr = modifiedStmts.begin(); itr != modifiedStmts.end(); itr++){
-          modifiedStmtIds.insert(*itr);
-        }
-      }
-      CollectionAssert::AreEqual(modifiedVarNames, allModifiedVars);
-      CollectionAssert::AreEqual(modifiedStmtIds, allModifiedStmts);
+	int stmtListCount = 3;
+	int stmtCount_ASSIGN = 3;
+	int stmtCount_READ = 1;
+	int stmtCount_PRINT = 1;
+	int stmtCount_CALL = 0;
+	int stmtCount_IF = 1;
+	int stmtCount_WHILE = 0;
 
-      std::unordered_set<VarId> usedVarIds = pkb.usesKB.getAllVarsUsedByProc(pkb.constTable.getProcId(procedureName));
-      std::unordered_set<VarName> usedVarNames;
-      std::unordered_set<StmtId> usedStmtIds;
-      for (auto itr = usedVarIds.begin(); itr != usedVarIds.end(); itr++){
-        usedVarNames.insert(pkb.varTable.get(*itr));
-        std::unordered_set<StmtId> usedStmts = pkb.usesKB.getAllStmtsUsingVar(*itr);
-        for (auto itr = usedStmts.begin(); itr != usedStmts.end(); itr++){
-          usedStmtIds.insert(*itr);
-        }
-      }
-      CollectionAssert::AreEqual(usedVarNames, allUsedVars);
-      CollectionAssert::AreEqual(usedStmtIds, allUsedStmts);
-
-      CollectionAssert::AreEqual(pkb.patternKB.getLHSPatternStmts("x"), std::unordered_set<StmtId>({1, 3}));
-      CollectionAssert::AreEqual(pkb.patternKB.getLHSPatternStmts("y"), std::unordered_set<StmtId>({2}));
-      CollectionAssert::AreEqual(pkb.patternKB.getLHSPatternStmts("z"), std::unordered_set<StmtId>({4}));
-      CollectionAssert::AreEqual(pkb.patternKB.getRHSPatternStmts("7"), std::unordered_set<StmtId>({1}));
-      CollectionAssert::AreEqual(pkb.patternKB.getRHSPatternStmts("2"), std::unordered_set<StmtId>({2}));
-      CollectionAssert::AreEqual(pkb.patternKB.getRHSPatternStmts("y"), std::unordered_set<StmtId>({3, 5, 6}));
-      CollectionAssert::AreEqual(pkb.patternKB.getRHSPatternStmts("x+a"), std::unordered_set<StmtId>({4}));
+	TEST_CLASS_INITIALIZE(setup) {
+		try {
+			pkb = PKB::PKB();
+			std::string src = "procedure p{x=7; y=2; if(x>y) then {z=x+a;} else {print y;} read y; }";
+			pkb = frontEnd.parseSimple(src);
 		}
+		catch (std::invalid_argument&) {
+			Assert::IsTrue(false);
+		}
+	}
 
+	TEST_METHOD(parseSimple) {
+		Assert::IsTrue(pkb.varTable.getAllVars() == allVars);
+
+		Assert::IsTrue(pkb.constTable.size() == allConstants.size());
+		std::unordered_set<ConstValue> constants;
+		for (auto itr = allConstants.begin(); itr != allConstants.end(); ++itr) {
+			try {
+				constants.insert(pkb.constTable.get(pkb.constTable.getConstId(*itr)));
+			}
+			catch (std::exception&) {
+				Assert::IsTrue(false);
+			}
+		}
+		Assert::IsTrue(constants == allConstants);
+
+		Assert::IsTrue(pkb.procTable.size() == 1);
+		Assert::IsFalse(pkb.procTable.getProcId(procedureName) == -1);
+
+		Assert::IsTrue(pkb.stmtListTable.size() == stmtListCount);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::ASSIGN).size() == stmtCount_ASSIGN);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::READ).size() == stmtCount_READ);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::PRINT).size() == stmtCount_PRINT);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::CALL).size() == stmtCount_CALL);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::IF).size() == stmtCount_IF);
+		Assert::IsTrue(pkb.stmtTable.getStmtsByType(StmtType::WHILE).size() == stmtCount_WHILE);
+
+		Assert::IsTrue(pkb.followsKB.follows(1, 2));
+		Assert::IsTrue(pkb.followsKB.follows(2, 3));
+		Assert::IsTrue(pkb.followsKB.follows(3, 6));
+		Assert::IsTrue(pkb.followsKB.followStar(1, 2));
+		Assert::IsTrue(pkb.followsKB.followStar(1, 3));
+		Assert::IsTrue(pkb.followsKB.followStar(1, 6));
+		Assert::IsTrue(pkb.followsKB.followStar(2, 3));
+		Assert::IsTrue(pkb.followsKB.followStar(2, 6));
+		Assert::IsTrue(pkb.followsKB.followStar(3, 6));
+
+		Assert::IsFalse(pkb.followsKB.follows(3, 4));
+		Assert::IsFalse(pkb.followsKB.follows(4, 5));
+		Assert::IsFalse(pkb.followsKB.follows(5, 6));
+		Assert::IsFalse(pkb.followsKB.followStar(1, 4));
+		Assert::IsFalse(pkb.followsKB.followStar(1, 5));
+		Assert::IsFalse(pkb.followsKB.followStar(2, 4));
+		Assert::IsFalse(pkb.followsKB.followStar(2, 5));
+		Assert::IsFalse(pkb.followsKB.followStar(3, 4));
+		Assert::IsFalse(pkb.followsKB.followStar(3, 5));
+		Assert::IsFalse(pkb.followsKB.followStar(4, 5));
+		Assert::IsFalse(pkb.followsKB.followStar(4, 6));
+		Assert::IsFalse(pkb.followsKB.followStar(5, 6));
+
+		Assert::IsTrue(pkb.parentKB.parent(3, 4));
+		Assert::IsTrue(pkb.parentKB.parent(3, 5));
+		Assert::IsTrue(pkb.parentKB.parentStar(3, 4));
+		Assert::IsTrue(pkb.parentKB.parentStar(3, 5));
+
+		Assert::IsFalse(pkb.parentKB.parent(1, 2));
+		Assert::IsFalse(pkb.parentKB.parent(2, 3));
+		Assert::IsFalse(pkb.parentKB.parent(5, 6));
+		Assert::IsFalse(pkb.parentKB.parentStar(1, 2));
+		Assert::IsFalse(pkb.parentKB.parentStar(1, 3));
+		Assert::IsFalse(pkb.parentKB.parentStar(1, 4));
+		Assert::IsFalse(pkb.parentKB.parentStar(1, 5));
+		Assert::IsFalse(pkb.parentKB.parentStar(1, 6));
+		Assert::IsFalse(pkb.parentKB.parentStar(2, 3));
+		Assert::IsFalse(pkb.parentKB.parentStar(2, 4));
+		Assert::IsFalse(pkb.parentKB.parentStar(2, 5));
+		Assert::IsFalse(pkb.parentKB.parentStar(2, 6));
+		Assert::IsFalse(pkb.parentKB.parentStar(3, 6));
+		Assert::IsFalse(pkb.parentKB.parentStar(4, 5));
+		Assert::IsFalse(pkb.parentKB.parentStar(4, 6));
+		Assert::IsFalse(pkb.parentKB.parentStar(5, 6));
+
+
+		std::unordered_set<VarId> modifiedVarIds = pkb.modifiesKB.getAllVarsModifiedByProc(pkb.procTable.getProcId(procedureName));
+		std::unordered_set<VarName> modifiedVarNames;
+		std::unordered_set<StmtId> modifiedStmtIds;
+		for (auto itr = modifiedVarIds.begin(); itr != modifiedVarIds.end(); itr++) {
+			modifiedVarNames.insert(pkb.varTable.get(*itr));
+			std::unordered_set<StmtId> modifiedStmts = pkb.modifiesKB.getAllStmtsModifyVar(*itr);
+			for (auto itr = modifiedStmts.begin(); itr != modifiedStmts.end(); itr++) {
+				modifiedStmtIds.insert(*itr);
+			}
+		}
+		Assert::IsTrue(modifiedVarNames == allModifiedVars);
+		Assert::IsTrue(modifiedStmtIds == allModifiedStmts);
+
+		std::unordered_set<VarId> usedVarIds = pkb.usesKB.getAllVarsUsedByProc(pkb.procTable.getProcId(procedureName));
+		std::unordered_set<VarName> usedVarNames;
+		std::unordered_set<StmtId> usedStmtIds;
+		for (auto itr = usedVarIds.begin(); itr != usedVarIds.end(); itr++) {
+			usedVarNames.insert(pkb.varTable.get(*itr));
+			std::unordered_set<StmtId> usedStmts = pkb.usesKB.getAllStmtsUsingVar(*itr);
+			for (auto itr = usedStmts.begin(); itr != usedStmts.end(); itr++) {
+				usedStmtIds.insert(*itr);
+			}
+		}
+		Assert::IsTrue(usedVarNames == allUsedVars);
+		Assert::IsTrue(usedStmtIds == allUsedStmts);
+
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("x") == std::unordered_set<StmtId>({ 1, 4 }));
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("y") == std::unordered_set<StmtId>({ 2 }));
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("z") == std::unordered_set<StmtId>({ 4 }));
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("7") == std::unordered_set<StmtId>({ 1 }));
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("2") == std::unordered_set<StmtId>({ 2 }));
+		Assert::IsTrue(pkb.patternKB.getAssignPatternStmts("x+a") == std::unordered_set<StmtId>({ 4 }));
+		Assert::IsTrue(pkb.patternKB.getIfPatternStmts(pkb.varTable.getVarId("x")) == std::unordered_set<StmtId>({ 3 }));
+		Assert::IsTrue(pkb.patternKB.getIfPatternStmts(pkb.varTable.getVarId("y")) == std::unordered_set<StmtId>({ 3 }));
+	}
 	};
 }
--- Team06/Code06/IntegrationTesting/test-PKB_PQL.cpp
+++ Team06/Code06/IntegrationTesting/test-PKB_PQL.cpp
@@ -1,50 +1,47 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "DesignExtractor.h"
-#include "PKB.h"
 #include "PQL.h"
 #include "Simple.h"
+
+using std::string;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace PQL;
 using namespace SIMPLE;
 
-namespace IntegrationTesting
-{		
-  PKB::PKB pkb;
-  PQL::PQLManager pql;
- 	FrontEnd::DesignExtractor DE;
-  std::list<std::string> results;
+namespace IntegrationTesting {
+    PQL::PQLManager pql = PQL::PQLManager(PKB::PKB());
+    FrontEnd::DesignExtractor DE;
+    std::list<string> results;
 
-	TEST_CLASS(Test-PKB_PQL)
-	{
-    std::string testQuery1 = "stmt s; Select s";
-    std::list<std::string> testQueryResult1 = {"1", "2", "3"};
-    std::string testQuery2 = "stmt s; Select s such that Follows(1, s)";
-    std::list<std::string> testQueryResult2 = {"2"};
-    std::string testQuery3 = "stmt s; Select s such that Parent(2, s)";
-    std::list<std::string> testQueryResult3 = {"3"};
+    TEST_CLASS(TestPKB_PQL) {
+    public:
+        string testQuery1 = "stmt s; Select s";
+        std::list<string> testQueryResult1 = {"1", "2", "3"};
+        std::string testQuery2 = "stmt s; Select s such that Follows(1, s)";
+        std::list<string> testQueryResult2 = {"2"};
+        std::string testQuery3 = "stmt s; Select s such that Parent(2, s)";
+        std::list<string> testQueryResult3 = {"3"};
 
-	public:
-		TEST_CLASS_INITIALIZE(setup) {
-      pkb = PKB::PKB();
-			pkb.stmtTable.insertStmt(&ReadStmt(1));
-			pkb.stmtTable.insertStmt(&ReadStmt(2));
-			pkb.stmtTable.insertStmt(&ReadStmt(3));
- 			pkb.parentKB.addParent(2, 3);
-      pkb = DE.run(pkb);
-      pql = PQL::PQLManager(pkb);
+    TEST_CLASS_INITIALIZE(setup) {
+        PKB::PKB pkb = PKB::PKB();
+        pkb.stmtTable.insertStmt(&ReadStmt(1));
+        pkb.stmtTable.insertStmt(&ReadStmt(2));
+        pkb.stmtTable.insertStmt(&ReadStmt(3));
+        pkb.parentKB.addParent(2, 3);
+        pkb = DE.run(pkb);
+        pql = PQL::PQLManager(pkb);
     }
-    
-		TEST_METHOD(evaluateQuery)
-		{
-      pql.evaluateQuery(testQuery1, &results);
-      CollectionAssert::AreEqual(results, testQueryResult1);
-      pql.evaluateQuery(testQuery2, &results);
-      CollectionAssert::AreEqual(results, testQueryResult2);
-      pql.evaluateQuery(testQuery3, &results);
-      CollectionAssert::AreEqual(results, testQueryResult3);
-		}
 
-	};
+    TEST_METHOD(evaluateQuery) {
+        pql.evaluateQuery(testQuery1, results);
+        Assert::IsTrue(results == testQueryResult1);
+        pql.evaluateQuery(testQuery2, results);
+        Assert::IsTrue(results == testQueryResult2);
+        pql.evaluateQuery(testQuery3, results);
+        Assert::IsTrue(results == testQueryResult3);
+    }
+
+  };
 }
