167f01667d673ed9b814bc93314c9fb1ad6c576c
laichengyu <laichengyu95@gmail.com>
2020-03-16 15:25:53 +0800

    Fix linting

--- Team06/Code06/source/NextKB.cpp
+++ Team06/Code06/source/NextKB.cpp
@@ -1,67 +1,71 @@
 #include "NextKB.h"
-#include <stdexcept>
 
-void NextKB::addNext(StmtId s1, StmtId s2)
-{
-	nextRS& nRS1 = nextTable[s1];
-	nextRS& nRS2 = nextTable[s2];
 
-	nRS1.directNext.insert(s2);
-	nRS2.directPrev.insert(s1);
 }
 
-bool NextKB::next(StmtId s1, StmtId s2)
-{
-	try {
-		std::unordered_set<ProcId> dirNext = nextTable.at(s1).directNext;
-		return dirNext.find(s2) != dirNext.end();
-	}
-	catch (const std::out_of_range&) {
-		return false;
-	}
+void NextKB::addNext(StmtId s1, StmtId s2) {
+    nextRS& nRS1 = nextTable[s1];
+    nextRS& nRS2 = nextTable[s2];
+
+    nRS1.directNext.insert(s2);
+    nRS2.directPrev.insert(s1);
 }
 
-const std::unordered_set<StmtId>& NextKB::getDirectNodes(StmtId s, NodeType type)
-{
-	try {
-		if (type == NodeType::SUCCESSOR) {
-			return nextTable.at(s).directNext;
-		}
-		else {
-			return nextTable.at(s).directPrev;
-		}
-	}
-	catch (const std::out_of_range&) {
-		return EMPTY_RESULT;
-	}
+bool NextKB::next(StmtId s1, StmtId s2) {
+    try {
+        std::unordered_set<ProcId> dirNext = nextTable.at(s1).directNext;
+        return dirNext.find(s2) != dirNext.end();
+    }
+    catch (const std::out_of_range&) {
+        return false;
+    }
 }
 
-bool NextKB::hasNext(StmtId s)
-{
-	try {
-		return nextTable.at(s).directNext.size() != 0;
-	}
-	catch (const std::out_of_range&) {
-		return false;
-	}
+bool NextKB::nextStar(StmtId s1, StmtId s2) {
+    if (nextStarTable.find(s1) == nextStarTable.end()) {
+        FrontEnd::processNextStar(s1, pkb);
+    }
+    std::unordered_set<StmtId> allNext = nextStarTable.at(s1).allNext;
+    return allNext.find(s2) != allNext.end();
 }
 
-bool NextKB::hasPrev(StmtId s)
-{
-	try {
-		return nextTable.at(s).directNext.size() != 0;
-	}
-	catch (const std::out_of_range&) {
-		return false;
-	}
+const std::unordered_set<StmtId>& NextKB::getDirectNodes(StmtId s, NodeType type) {
+    try {
+        if (type == NodeType::SUCCESSOR) {
+            return nextTable.at(s).directNext;
+        } else {
+            return nextTable.at(s).directPrev;
+        }
+    }
+    catch (const std::out_of_range&) {
+        return EMPTY_RESULT;
+    }
 }
 
-bool NextKB::hasNextRelation()
-{
-	return nextTable.size() != 0;
 }
 
-void NextKB::clear()
-{
-	nextStarTable.clear();
+bool NextKB::hasNext(StmtId s) {
+    try {
+        return nextTable.at(s).directNext.size() != 0;
+    }
+    catch (const std::out_of_range&) {
+        return false;
+    }
+}
+
+bool NextKB::hasPrev(StmtId s) {
+    try {
+        return nextTable.at(s).directNext.size() != 0;
+    }
+    catch (const std::out_of_range&) {
+        return false;
+    }
+}
+
+bool NextKB::hasNextRelation() {
+    return nextTable.size() != 0;
+}
+
+void NextKB::clear() {
+    nextStarTable.clear();
 }
--- Team06/Code06/source/NextKB.h
+++ Team06/Code06/source/NextKB.h
@@ -1,61 +1,61 @@
 #pragma once
 
-#include "unordered_set"
+#include <stdexcept>
 #include "unordered_map"
+#include "unordered_set"
 
 #include "Types.h"
 #include "Constants.h"
 
 struct nextRS {
-	std::unordered_set<StmtId> directPrev;
-	std::unordered_set<StmtId> directNext;
+    std::unordered_set<StmtId> directPrev;
+    std::unordered_set<StmtId> directNext;
 
 };
 
 struct nextStarRS {
-	std::unordered_set<StmtId> allPrev;
-	std::unordered_set<StmtId> allNext;
+    std::unordered_set<StmtId> allPrev;
+    std::unordered_set<StmtId> allNext;
 };
 
-class NextKB
-{
-public:
-	/*
-		Adds Next(s1, s2) to the nextTable where s2 can be executed immediately after s1
-	*/
-	void addNext(StmtId s1, StmtId s2);
+class NextKB {
+ public:
+    /*
+        Adds Next(s1, s2) to the nextTable where s2 can be executed immediately after s1
+    */
+    void addNext(StmtId s1, StmtId s2);
 
-	/*
-		Returns TRUE if s2 can be executed immediately after s1, FALSE otherwise
-	*/
-	bool next(StmtId s1, StmtId s2);
+    /*
+        Returns TRUE if s2 can be executed immediately after s1, FALSE otherwise
+    */
+    bool next(StmtId s1, StmtId s2);
 
-	/*
-		Returns a reference to directNext/directPrev of s for NodeType SUCCESSOR and PREDECESSOR respectively
-	*/
-	const std::unordered_set<StmtId>& getDirectNodes(StmtId s, NodeType type);
+    /*
+        Returns a reference to directNext/directPrev of s for NodeType SUCCESSOR and PREDECESSOR respectively
+    */
+    const std::unordered_set<StmtId>& getDirectNodes(StmtId s, NodeType type);
 
-	/*
-		Returns TRUE if directNext of s is non-empty, FALSE otherwise
-	*/
-	bool hasNext(StmtId s);
+    /*
+        Returns TRUE if directNext of s is non-empty, FALSE otherwise
+    */
+    bool hasNext(StmtId s);
 
-	/*
-		Returns TRUE if directPrev of s is non-empty, FALSE otherwise
-	*/
-	bool hasPrev(StmtId s);
+    /*
+        Returns TRUE if directPrev of s is non-empty, FALSE otherwise
+    */
+    bool hasPrev(StmtId s);
 
-	/*
-		Returns TRUE if a Next relation exists, FALSE otherwise
-	*/
-	bool hasNextRelation();
+    /*
+        Returns TRUE if a Next relation exists, FALSE otherwise
+    */
+    bool hasNextRelation();
 
-	/*
-		Clears the nextStarTable
-	*/
-	void clear();
+    /*
+        Clears the nextStarTable
+    */
+    void clear();
 
-private:
-	std::unordered_map<StmtId, nextRS> nextTable;
-	std::unordered_map<StmtId, nextStarRS> nextStarTable;
+ private:
+    std::unordered_map<StmtId, nextRS> nextTable;
+    std::unordered_map<StmtId, nextStarRS> nextStarTable;
 };
