e14e7e239e1462a2d5da9ab683fd0c5552ab27e1
Lee Liak Ghee <liakify@gmail.com>
2020-03-16 14:33:00 +0800

    Fix tabs

--- Team06/Code06/UnitTesting/testPatternKB.cpp
+++ Team06/Code06/UnitTesting/testPatternKB.cpp
@@ -6,46 +6,46 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTesting
 {
-	PatternKB patternKB;
+    PatternKB patternKB;
 
-	TEST_CLASS(TestPatternKB)
-	{
-	public:
-		TEST_CLASS_INITIALIZE(setup) {
-			patternKB.addAssignPattern("a", 1);
-			patternKB.addAssignPattern("b", 1);
-			patternKB.addAssignPattern("a", 2);
+    TEST_CLASS(TestPatternKB)
+    {
+    public:
+        TEST_CLASS_INITIALIZE(setup) {
+            patternKB.addAssignPattern("a", 1);
+            patternKB.addAssignPattern("b", 1);
+            patternKB.addAssignPattern("a", 2);
 
-			patternKB.addIfPattern(1, 1);
-			patternKB.addIfPattern(2, 1);
-			patternKB.addIfPattern(1, 2);
+            patternKB.addIfPattern(1, 1);
+            patternKB.addIfPattern(2, 1);
+            patternKB.addIfPattern(1, 2);
 
-			patternKB.addWhilePattern(1, 1);
-			patternKB.addWhilePattern(2, 1);
-			patternKB.addWhilePattern(1, 2);
-		}
+            patternKB.addWhilePattern(1, 1);
+            patternKB.addWhilePattern(2, 1);
+            patternKB.addWhilePattern(1, 2);
+        }
 
-		TEST_METHOD(getAssignPatternStmts) {
-			Assert::IsTrue(patternKB.getAssignPatternStmts("a") == std::unordered_set<StmtId>({ 1, 2 }));
-			Assert::IsTrue(patternKB.getAssignPatternStmts("b") == std::unordered_set<StmtId>({ 1 }));
-			Assert::IsTrue(patternKB.getAssignPatternStmts("") == std::unordered_set<StmtId>());
-			Assert::IsTrue(patternKB.getAssignPatternStmts("c") == std::unordered_set<StmtId>());
-		}
+        TEST_METHOD(getAssignPatternStmts) {
+            Assert::IsTrue(patternKB.getAssignPatternStmts("a") == std::unordered_set<StmtId>({ 1, 2 }));
+            Assert::IsTrue(patternKB.getAssignPatternStmts("b") == std::unordered_set<StmtId>({ 1 }));
+            Assert::IsTrue(patternKB.getAssignPatternStmts("") == std::unordered_set<StmtId>());
+            Assert::IsTrue(patternKB.getAssignPatternStmts("c") == std::unordered_set<StmtId>());
+        }
 
-		TEST_METHOD(getIfPatternStmts) {
-			Assert::IsTrue(patternKB.getIfPatternStmts(1) == std::unordered_set<StmtId>({ 1, 2 }));
-			Assert::IsTrue(patternKB.getIfPatternStmts(2) == std::unordered_set<StmtId>({ 1 }));
-			Assert::IsTrue(patternKB.getIfPatternStmts(-1) == std::unordered_set<StmtId>());
-			Assert::IsTrue(patternKB.getIfPatternStmts(0) == std::unordered_set<StmtId>());
-			Assert::IsTrue(patternKB.getIfPatternStmts(3) == std::unordered_set<StmtId>());
-		}
+        TEST_METHOD(getIfPatternStmts) {
+            Assert::IsTrue(patternKB.getIfPatternStmts(1) == std::unordered_set<StmtId>({ 1, 2 }));
+            Assert::IsTrue(patternKB.getIfPatternStmts(2) == std::unordered_set<StmtId>({ 1 }));
+            Assert::IsTrue(patternKB.getIfPatternStmts(-1) == std::unordered_set<StmtId>());
+            Assert::IsTrue(patternKB.getIfPatternStmts(0) == std::unordered_set<StmtId>());
+            Assert::IsTrue(patternKB.getIfPatternStmts(3) == std::unordered_set<StmtId>());
+        }
 
-		TEST_METHOD(getWhilePatternStmts) {
-			Assert::IsTrue(patternKB.getWhilePatternStmts(1) == std::unordered_set<StmtId>({ 1, 2 }));
-			Assert::IsTrue(patternKB.getWhilePatternStmts(2) == std::unordered_set<StmtId>({ 1 }));
-			Assert::IsTrue(patternKB.getIfPatternStmts(-1) == std::unordered_set<StmtId>());
-			Assert::IsTrue(patternKB.getWhilePatternStmts(0) == std::unordered_set<StmtId>());
-			Assert::IsTrue(patternKB.getWhilePatternStmts(3) == std::unordered_set<StmtId>());
-		}
-	};
+        TEST_METHOD(getWhilePatternStmts) {
+            Assert::IsTrue(patternKB.getWhilePatternStmts(1) == std::unordered_set<StmtId>({ 1, 2 }));
+            Assert::IsTrue(patternKB.getWhilePatternStmts(2) == std::unordered_set<StmtId>({ 1 }));
+            Assert::IsTrue(patternKB.getIfPatternStmts(-1) == std::unordered_set<StmtId>());
+            Assert::IsTrue(patternKB.getWhilePatternStmts(0) == std::unordered_set<StmtId>());
+            Assert::IsTrue(patternKB.getWhilePatternStmts(3) == std::unordered_set<StmtId>());
+        }
+    };
 }
--- Team06/Code06/UnitTesting/testStmtTable.cpp
+++ Team06/Code06/UnitTesting/testStmtTable.cpp
@@ -8,54 +8,54 @@ using namespace SIMPLE;
 
 namespace UnitTesting
 {
-	TEST_CLASS(TestStmtTable)
-	{
-	public:
-		VarId VAR_ID = 1;
-		std::shared_ptr<Statement> STMT_A = std::shared_ptr<Statement>(new PrintStmt(VAR_ID));
-		std::shared_ptr<Statement> STMT_B = std::shared_ptr<Statement>(new ReadStmt(VAR_ID));
-		StmtId STMT_ID_A = 0;
-		StmtId STMT_ID_B = 1;
-		bool isExceptionThrown = false;
+    TEST_CLASS(TestStmtTable)
+    {
+    public:
+        VarId VAR_ID = 1;
+        std::shared_ptr<Statement> STMT_A = std::shared_ptr<Statement>(new PrintStmt(VAR_ID));
+        std::shared_ptr<Statement> STMT_B = std::shared_ptr<Statement>(new ReadStmt(VAR_ID));
+        StmtId STMT_ID_A = 0;
+        StmtId STMT_ID_B = 1;
+        bool isExceptionThrown = false;
 
-		TEST_METHOD(InitializingStmtTable)
-		{
-			PKB::StmtTable stmtTable;
+        TEST_METHOD(InitializingStmtTable)
+        {
+            PKB::StmtTable stmtTable;
 
-			Assert::AreEqual(stmtTable.size(), 0);
-			try {
-				stmtTable.get(STMT_ID_A);
-			}
-			catch (out_of_range &) {
-				isExceptionThrown = true;
-			}
-			Assert::IsTrue(isExceptionThrown);
-			isExceptionThrown = false;
-			Assert::IsTrue(stmtTable.getStmtsByType(StmtType::PRINT).empty());
-		}
+            Assert::AreEqual(stmtTable.size(), 0);
+            try {
+                stmtTable.get(STMT_ID_A);
+            }
+            catch (out_of_range &) {
+                isExceptionThrown = true;
+            }
+            Assert::IsTrue(isExceptionThrown);
+            isExceptionThrown = false;
+            Assert::IsTrue(stmtTable.getStmtsByType(StmtType::PRINT).empty());
+        }
 
-		TEST_METHOD(StmtTableFunctions) {
+        TEST_METHOD(StmtTableFunctions) {
 
-			PKB::StmtTable stmtTable;
+            PKB::StmtTable stmtTable;
 
-			StmtId stmt_A_id = stmtTable.insertStmt(STMT_A);
-			Assert::AreEqual(stmtTable.size(), 1);
+            StmtId stmt_A_id = stmtTable.insertStmt(STMT_A);
+            Assert::AreEqual(stmtTable.size(), 1);
 
-			std::unordered_set<StmtId> set = stmtTable.getStmtsByType(StmtType::PRINT);
+            std::unordered_set<StmtId> set = stmtTable.getStmtsByType(StmtType::PRINT);
 
-			Assert::IsFalse(set.empty());
-			Assert::IsFalse(set.find(stmt_A_id) == set.end());
+            Assert::IsFalse(set.empty());
+            Assert::IsFalse(set.find(stmt_A_id) == set.end());
 
-			Assert::ExpectException<std::invalid_argument>([&stmtTable, this] {stmtTable.insertStmtAtId(STMT_B, 1); });
-			StmtId reserved = stmtTable.reserveId();
-			StmtId stmt_B_id = stmtTable.insertStmt(STMT_B);
-			Assert::IsTrue(set.find(reserved) == set.end());
-			try {
-				stmtTable.insertStmtAtId(STMT_B, reserved);
-			}
-			catch (std::invalid_argument&) {
-				Assert::IsTrue(false);
-			}
-		}
-	};
+            Assert::ExpectException<std::invalid_argument>([&stmtTable, this] {stmtTable.insertStmtAtId(STMT_B, 1); });
+            StmtId reserved = stmtTable.reserveId();
+            StmtId stmt_B_id = stmtTable.insertStmt(STMT_B);
+            Assert::IsTrue(set.find(reserved) == set.end());
+            try {
+                stmtTable.insertStmtAtId(STMT_B, reserved);
+            }
+            catch (std::invalid_argument&) {
+                Assert::IsTrue(false);
+            }
+        }
+    };
 }
