5b58fbd11bf972c0522a0933f206603c265013ae
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-03-17 14:50:44 +0800

    PKB-PQL Test: add select <design entity> queries

--- Team06/Code06/IntegrationTesting/testPKB_PQL.cpp
+++ Team06/Code06/IntegrationTesting/testPKB_PQL.cpp
@@ -15,7 +15,20 @@ namespace IntegrationTesting {
 
     TEST_CLASS(TestPKB_PQL) {
     public:
-        string testQuery = "stmt s; Select s";
+
+        string select_DesignEntity_STMT = "stmt s; Select s";
+        string select_DesignEntity_READ = "read r; Select r";
+        string select_DesignEntity_PRINT = "print pn; Select pn";
+        string select_DesignEntity_CALL = "call c; Select c";
+        string select_DesignEntity_WHILE = "while w; Select w";
+        string select_DesignEntity_IF = "if ifs; Select ifs";
+        string select_DesignEntity_ASSIGN = "assign a; Select a";
+        string select_DesignEntity_VAR = "variable v; Select v";
+        string select_DesignEntity_CONST = "variable v; Select v";
+        string select_DesignEntity_PROGLINE = "prog_line n; Select n";
+        string select_DesignEntity_PROC = "procedure p; Select p";
+        
+        
         std::list<string> testQueryResult = {"1", "2", "3"};
 
     TEST_CLASS_INITIALIZE(setup) {
