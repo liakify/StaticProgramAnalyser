ad03169d7e9139c55053833724b765cd43fb07c2
laichengyu <laichengyu95@gmail.com>
2020-03-19 14:54:54 +0800

    Add more NextKB unit tests

--- Team06/Code06/UnitTesting/testNextKB.cpp
+++ Team06/Code06/UnitTesting/testNextKB.cpp
@@ -6,121 +6,138 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTesting {
-    NextKB nKB = NextKB(&PKB::PKB());
+    PKB::PKB pkb = PKB::PKB();
 
     TEST_CLASS(TestNextKB) {
     public:
         std::unordered_set<StmtId> emptyResult;
 
         TEST_CLASS_INITIALIZE(setup) {
-            nKB.addNext(1, 2);
-            nKB.addNext(2, 3);
-            nKB.addToAll(1, 2, NodeType::SUCCESSOR);
-            nKB.addToAll(1, 3, NodeType::SUCCESSOR);
-            nKB.addToAll(2, 3, NodeType::SUCCESSOR);
-            nKB.addToAll(2, 1, NodeType::PREDECESSOR);
-            nKB.addToAll(3, 2, NodeType::PREDECESSOR);
-            nKB.addToAll(3, 1, NodeType::PREDECESSOR);
+            pkb.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(1)));
+            pkb.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(2)));
+            pkb.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(3)));
+            pkb.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(4)));
+
+            pkb.nextKB.addNext(1, 2);
+            pkb.nextKB.addNext(2, 3);
+            pkb.nextKB.addToAll(1, 2, NodeType::SUCCESSOR);
+            pkb.nextKB.addToAll(1, 3, NodeType::SUCCESSOR);
+            pkb.nextKB.addToAll(2, 3, NodeType::SUCCESSOR);
+            pkb.nextKB.addToAll(2, 1, NodeType::PREDECESSOR);
+            pkb.nextKB.addToAll(3, 2, NodeType::PREDECESSOR);
+            pkb.nextKB.addToAll(3, 1, NodeType::PREDECESSOR);
+            pkb.nextKB.setProcessedAll(1, NodeType::SUCCESSOR);
+            pkb.nextKB.setProcessedAll(2, NodeType::SUCCESSOR);
+            pkb.nextKB.setProcessedAll(3, NodeType::SUCCESSOR);
+            pkb.nextKB.setProcessedAll(3, NodeType::PREDECESSOR);
+            pkb.nextKB.setProcessedAll(2, NodeType::PREDECESSOR);
+            pkb.nextKB.setProcessedAll(1, NodeType::PREDECESSOR);
         }
 
         TEST_METHOD(addNext) {
-            Assert::IsTrue(nKB.getDirectNodes(1, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{2});
-            Assert::IsTrue(nKB.getDirectNodes(2, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{3});
-            Assert::IsTrue(nKB.getDirectNodes(3, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(1, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{2});
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(2, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{3});
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(3, NodeType::SUCCESSOR) == emptyResult);
 
-            Assert::IsTrue(nKB.getDirectNodes(1, NodeType::PREDECESSOR) == emptyResult);
-            Assert::IsTrue(nKB.getDirectNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
-            Assert::IsTrue(nKB.getDirectNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{2});
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(1, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{2});
         }
 
         TEST_METHOD(next) {
-            Assert::IsTrue(nKB.next(1, 2));
-            Assert::IsTrue(nKB.next(2, 3));
-            Assert::IsFalse(nKB.next(1, 3));
+            Assert::IsTrue(pkb.nextKB.next(1, 2));
+            Assert::IsTrue(pkb.nextKB.next(2, 3));
+            Assert::IsFalse(pkb.nextKB.next(1, 3));
 
-            Assert::IsFalse(nKB.next(2, 1));
-            Assert::IsFalse(nKB.next(3, 1));
-            Assert::IsFalse(nKB.next(3, 2));
+            Assert::IsFalse(pkb.nextKB.next(2, 1));
+            Assert::IsFalse(pkb.nextKB.next(3, 1));
+            Assert::IsFalse(pkb.nextKB.next(3, 2));
 
-            Assert::IsFalse(nKB.next(0, 1));
-            Assert::IsFalse(nKB.next(1, -1));
-            Assert::IsFalse(nKB.next(4, -1));
+            Assert::IsFalse(pkb.nextKB.next(0, 1));
+            Assert::IsFalse(pkb.nextKB.next(1, -1));
+            Assert::IsFalse(pkb.nextKB.next(5, -1));
         }
 
-        //TEST_METHOD(nextStar) {
-        //    Assert::IsTrue(nKB.nextStar(1, 2));
-        //    Assert::IsTrue(nKB.nextStar(1, 3));
-        //    Assert::IsTrue(nKB.nextStar(2, 3));
+        TEST_METHOD(nextStar) {
+            Assert::IsTrue(pkb.nextKB.nextStar(1, 2));
+            Assert::IsTrue(pkb.nextKB.nextStar(1, 3));
+            Assert::IsTrue(pkb.nextKB.nextStar(2, 3));
 
-        //    Assert::IsFalse(nKB.nextStar(2, 1));
-        //    Assert::IsFalse(nKB.nextStar(3, 1));
-        //    Assert::IsFalse(nKB.nextStar(3, 2));
+            Assert::IsFalse(pkb.nextKB.nextStar(2, 1));
+            Assert::IsFalse(pkb.nextKB.nextStar(3, 1));
+            Assert::IsFalse(pkb.nextKB.nextStar(3, 2));
 
-        //    Assert::IsFalse(nKB.nextStar(0, 1));
-        //    Assert::IsFalse(nKB.nextStar(1, -1));
-        //    Assert::IsFalse(nKB.nextStar(4, -1));
-        //}
+            Assert::IsFalse(pkb.nextKB.nextStar(0, 1));
+            Assert::IsFalse(pkb.nextKB.nextStar(1, -1));
+            Assert::IsFalse(pkb.nextKB.nextStar(5, -1));
+        }
 
         TEST_METHOD(getDirectNodes) {
-            Assert::IsTrue(nKB.getDirectNodes(0, NodeType::SUCCESSOR) == emptyResult);
-            Assert::IsTrue(nKB.getDirectNodes(4, NodeType::SUCCESSOR) == emptyResult);
-            Assert::IsTrue(nKB.getDirectNodes(-1, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(0, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(5, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(-1, NodeType::SUCCESSOR) == emptyResult);
 
-            Assert::IsTrue(nKB.getDirectNodes(0, NodeType::PREDECESSOR) == emptyResult);
-            Assert::IsTrue(nKB.getDirectNodes(4, NodeType::PREDECESSOR) == emptyResult);
-            Assert::IsTrue(nKB.getDirectNodes(-1, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(0, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(5, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getDirectNodes(-1, NodeType::PREDECESSOR) == emptyResult);
         }
 
-        //TEST_METHOD(getAllNodes) {
-        //    Assert::IsTrue(nKB.getAllNodes(0, NodeType::SUCCESSOR) == emptyResult);
-        //    Assert::IsTrue(nKB.getAllNodes(4, NodeType::SUCCESSOR) == emptyResult);
-        //    Assert::IsTrue(nKB.getAllNodes(-1, NodeType::SUCCESSOR) == emptyResult);
+        TEST_METHOD(getAllNodes) {
+            Assert::IsTrue(pkb.nextKB.getAllNodes(0, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getAllNodes(5, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getAllNodes(-1, NodeType::SUCCESSOR) == emptyResult);
 
-        //    Assert::IsTrue(nKB.getAllNodes(0, NodeType::PREDECESSOR) == emptyResult);
-        //    Assert::IsTrue(nKB.getAllNodes(4, NodeType::PREDECESSOR) == emptyResult);
-        //    Assert::IsTrue(nKB.getAllNodes(-1, NodeType::PREDECESSOR) == emptyResult);
-        //}
+            Assert::IsTrue(pkb.nextKB.getAllNodes(0, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getAllNodes(5, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(pkb.nextKB.getAllNodes(-1, NodeType::PREDECESSOR) == emptyResult);
+        }
 
-        //TEST_METHOD(addToAll_single) {
-        //    Assert::IsTrue(nKB.getAllNodes(2, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{3});
-        //    Assert::IsTrue(nKB.getAllNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
-        //}
+        TEST_METHOD(addToAll) {
+            Assert::IsTrue(pkb.nextKB.getAllNodes(1, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{2, 3});
+            Assert::IsTrue(pkb.nextKB.getAllNodes(1, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{});
 
-        //TEST_METHOD(addToAll_many) {
-        //    Assert::IsTrue(nKB.getAllNodes(1, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{2, 3});
-        //    Assert::IsTrue(nKB.getAllNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1, 2});
-        //}
+            Assert::IsTrue(pkb.nextKB.getAllNodes(2, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{3});
+            Assert::IsTrue(pkb.nextKB.getAllNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
+
+            Assert::IsTrue(pkb.nextKB.getAllNodes(3, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{});
+            Assert::IsTrue(pkb.nextKB.getAllNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1, 2});
+        }
 
         TEST_METHOD(hasNext) {
-            Assert::IsTrue(nKB.hasNext(1));
-            Assert::IsTrue(nKB.hasNext(2));
-            Assert::IsFalse(nKB.hasNext(3));
+            Assert::IsTrue(pkb.nextKB.hasNext(1));
+            Assert::IsTrue(pkb.nextKB.hasNext(2));
+            Assert::IsFalse(pkb.nextKB.hasNext(3));
 
-            Assert::IsFalse(nKB.hasNext(0));
-            Assert::IsFalse(nKB.hasNext(-1));
-            Assert::IsFalse(nKB.hasNext(4));
+            Assert::IsFalse(pkb.nextKB.hasNext(0));
+            Assert::IsFalse(pkb.nextKB.hasNext(-1));
+            Assert::IsFalse(pkb.nextKB.hasNext(5));
         }
 
         TEST_METHOD(hasPrev) {
-            Assert::IsFalse(nKB.hasPrev(1));
-            Assert::IsTrue(nKB.hasPrev(2));
-            Assert::IsTrue(nKB.hasPrev(3));
+            Assert::IsFalse(pkb.nextKB.hasPrev(1));
+            Assert::IsTrue(pkb.nextKB.hasPrev(2));
+            Assert::IsTrue(pkb.nextKB.hasPrev(3));
 
-            Assert::IsFalse(nKB.hasPrev(0));
-            Assert::IsFalse(nKB.hasPrev(-1));
-            Assert::IsFalse(nKB.hasPrev(4));
+            Assert::IsFalse(pkb.nextKB.hasPrev(0));
+            Assert::IsFalse(pkb.nextKB.hasPrev(-1));
+            Assert::IsFalse(pkb.nextKB.hasPrev(5));
         }
 
         TEST_METHOD(hasNextRelation) {
-            Assert::IsTrue(nKB.hasNextRelation());
+            Assert::IsTrue(pkb.nextKB.hasNextRelation());
         }
 
-        //TEST_METHOD(getAllNodes) {
-        //    Assert::IsTrue(nKB.getAllNodes() == std::unordered_set<StmtId>{1, 2});
-        //}
+        TEST_METHOD(setProcessedAll) {
+            Assert::IsTrue(pkb.nextKB.processedAll(1, NodeType::SUCCESSOR));
+            Assert::IsTrue(pkb.nextKB.processedAll(2, NodeType::SUCCESSOR));
+            Assert::IsTrue(pkb.nextKB.processedAll(3, NodeType::SUCCESSOR));
 
-        //TEST_METHOD(getAllNodes) {
-        //    Assert::IsTrue(nKB.getAllNodes() == std::unordered_set<StmtId>{2, 3});
-        //}
+            Assert::IsTrue(pkb.nextKB.processedAll(1, NodeType::PREDECESSOR));
+            Assert::IsTrue(pkb.nextKB.processedAll(2, NodeType::PREDECESSOR));
+            Assert::IsTrue(pkb.nextKB.processedAll(3, NodeType::PREDECESSOR));
+
+            Assert::IsFalse(pkb.nextKB.processedAll(4, NodeType::SUCCESSOR));
+            Assert::IsFalse(pkb.nextKB.processedAll(4, NodeType::PREDECESSOR));
+        }
     };
 }
