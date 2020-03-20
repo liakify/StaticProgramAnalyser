9b9c3a869ec4a21656d165144f639b2cd340ddeb
laichengyu <laichengyu95@gmail.com>
2020-03-20 16:44:18 +0800

    Update nextStar tests

--- Team06/Code06/UnitTesting/testRuntimeDesignExtractor_nextStar.cpp
+++ Team06/Code06/UnitTesting/testRuntimeDesignExtractor_nextStar.cpp
@@ -75,27 +75,29 @@ namespace UnitTesting {
             procedure e {
             30.    read;
             31.    if (cond) then {
-                       read;
-            32.        if (cond) then {
-            33.            read;
-                       } else {
+            32.        read;
+            33.        if (cond) then {
             34.            read;
+                       } else {
+            35.            read;
                        }
                    } else {
-            35.        if (cond) then {
-            36.            read;
-                       } else {
+            36.        if (cond) then {
             37.            read;
+                       } else {
+            38.            read;
                        }
-                       read;
+            39.        read;
                    }
-            38.    read;
+            40.    read;
             }
             */
             pkbNextStar = PKB::PKB();
             pkbNextStar.procTable.insertProc(Procedure("a", 1));
             pkbNextStar.procTable.insertProc(Procedure("b", 2));
             pkbNextStar.procTable.insertProc(Procedure("c", 10));
+            pkbNextStar.procTable.insertProc(Procedure("d", 17));
+            pkbNextStar.procTable.insertProc(Procedure("e", 20));
 
             StatementList sl1 = StatementList(std::vector<StmtId>{1});
             pkbNextStar.stmtListTable.insertStmtLst(sl1);
@@ -129,6 +131,26 @@ namespace UnitTesting {
             pkbNextStar.stmtListTable.insertStmtLst(sl15);
             StatementList sl16 = StatementList(std::vector<StmtId>{24});
             pkbNextStar.stmtListTable.insertStmtLst(sl16);
+            StatementList sl17 = StatementList(std::vector<StmtId>{25});
+            pkbNextStar.stmtListTable.insertStmtLst(sl17);
+            StatementList sl18 = StatementList(std::vector<StmtId>{26, 27, 29});
+            pkbNextStar.stmtListTable.insertStmtLst(sl18);
+            StatementList sl19 = StatementList(std::vector<StmtId>{28});
+            pkbNextStar.stmtListTable.insertStmtLst(sl19);
+            StatementList sl20 = StatementList(std::vector<StmtId>{30, 31, 40});
+            pkbNextStar.stmtListTable.insertStmtLst(sl20);
+            StatementList sl21 = StatementList(std::vector<StmtId>{32, 33});
+            pkbNextStar.stmtListTable.insertStmtLst(sl21);
+            StatementList sl22 = StatementList(std::vector<StmtId>{34});
+            pkbNextStar.stmtListTable.insertStmtLst(sl22);
+            StatementList sl23 = StatementList(std::vector<StmtId>{35});
+            pkbNextStar.stmtListTable.insertStmtLst(sl23);
+            StatementList sl24 = StatementList(std::vector<StmtId>{36, 39});
+            pkbNextStar.stmtListTable.insertStmtLst(sl24);
+            StatementList sl25 = StatementList(std::vector<StmtId>{37});
+            pkbNextStar.stmtListTable.insertStmtLst(sl25);
+            StatementList sl26 = StatementList(std::vector<StmtId>{38});
+            pkbNextStar.stmtListTable.insertStmtLst(sl26);
 
             CondExpr cond = CondExpr(Expression("1", 1, ExprType::CONST), Expression("2", 2, ExprType::CONST));
 
@@ -156,6 +178,22 @@ namespace UnitTesting {
             pkbNextStar.stmtTable.insertStmt(std::shared_ptr<WhileStmt>(new WhileStmt(cond, 15)));
             pkbNextStar.stmtTable.insertStmt(std::shared_ptr<WhileStmt>(new WhileStmt(cond, 16)));
             pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(24)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<WhileStmt>(new WhileStmt(cond, 18)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(26)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<WhileStmt>(new WhileStmt(cond, 19)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(28)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(29)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(30)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<IfStmt>(new IfStmt(cond, 21, 24)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(32)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<IfStmt>(new IfStmt(cond, 22, 23)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(34)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(35)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<IfStmt>(new IfStmt(cond, 25, 26)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(37)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(38)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(39)));
+            pkbNextStar.stmtTable.insertStmt(std::shared_ptr<ReadStmt>(new ReadStmt(40)));
 
             pkbNextStar = DE_nextStar.run(pkbNextStar);
         }
@@ -239,7 +277,87 @@ namespace UnitTesting {
                 }
             }
 
-            
+            for (int j = 25; j <= 29; j++) {
+                for (int i = j; i <= numStmts; i++) {
+                    if (i <= 29) {
+                        Assert::IsTrue(pkbNextStar.nextStar(j, i));
+                        Assert::IsTrue(pkbNextStar.nextStar(i, j));
+                    } else {
+                        Assert::IsFalse(pkbNextStar.nextStar(j, i));
+                        Assert::IsFalse(pkbNextStar.nextStar(i, j));
+                    }
+                }
+            }
+
+            StmtId procEFirstStmt = 30;
+            StmtId procELastStmt = 40;
+
+            for (int j = 30; j <= 31; j++) {
+                Assert::IsFalse(pkbNextStar.nextStar(j, j));
+                for (int i = j + 1; i <= numStmts; i++) {
+                    if (i <= procELastStmt) {
+                        Assert::IsTrue(pkbNextStar.nextStar(j, i));
+                    } else {
+                        Assert::IsFalse(pkbNextStar.nextStar(j, i));
+                    }
+                    Assert::IsFalse(pkbNextStar.nextStar(i, j));
+                }
+            }
+
+            for (int j = 32; j <= 33; j++) {
+                Assert::IsFalse(pkbNextStar.nextStar(j, j));
+                for (int i = j + 1; i <= numStmts; i++) {
+                    if (i <= 35 || i == 40) {
+                        Assert::IsTrue(pkbNextStar.nextStar(j, i));
+                    } else {
+                        Assert::IsFalse(pkbNextStar.nextStar(j, i));
+                    }
+                    Assert::IsFalse(pkbNextStar.nextStar(i, j));
+                }
+            }
+
+            for (int j = 34; j <= 35; j++) {
+                Assert::IsFalse(pkbNextStar.nextStar(j, j));
+                for (int i = j + 1; i <= numStmts; i++) {
+                    if (i == procELastStmt) {
+                        Assert::IsTrue(pkbNextStar.nextStar(j, i));
+                    } else {
+                        Assert::IsFalse(pkbNextStar.nextStar(j, i));
+                    }
+                    Assert::IsFalse(pkbNextStar.nextStar(i, j));
+                }
+            }
+
+            Assert::IsFalse(pkbNextStar.nextStar(36, 36));
+            for (int i = 37; i <= numStmts; i++) {
+                if (i <= procELastStmt) {
+                    Assert::IsTrue(pkbNextStar.nextStar(36, i));
+                } else {
+                    Assert::IsFalse(pkbNextStar.nextStar(36, i));
+                }
+                Assert::IsFalse(pkbNextStar.nextStar(i, 36));
+            }
+
+            for (int j = 37; j <= 38; j++) {
+                Assert::IsFalse(pkbNextStar.nextStar(j, j));
+                for (int i = j + 1; i <= numStmts; i++) {
+                    if (i == procELastStmt || i == procELastStmt - 1) {
+                        Assert::IsTrue(pkbNextStar.nextStar(j, i));
+                    } else {
+                        Assert::IsFalse(pkbNextStar.nextStar(j, i));
+                    }
+                    Assert::IsFalse(pkbNextStar.nextStar(i, j));
+                }
+            }
+
+            Assert::IsTrue(pkbNextStar.nextStar(39, 40));
+            Assert::IsFalse(pkbNextStar.nextStar(40, 39));
+
+            Assert::IsFalse(pkbNextStar.nextStar(40, 40));
+
+            Assert::IsFalse(pkbNextStar.nextStar(0, 40));
+            Assert::IsFalse(pkbNextStar.nextStar(40, -1));
+            Assert::IsFalse(pkbNextStar.nextStar(41, -1));
         }
 
         TEST_METHOD(processStmtAllNodes) {
@@ -325,14 +443,31 @@ namespace UnitTesting {
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(24, NodeType::SUCCESSOR) == allAftElse);
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(24, NodeType::PREDECESSOR) == allAftElseInclIf);
 
+            std::unordered_set<StmtId> procDWhile = { 25, 26, 27, 28, 29 };
+
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(25, NodeType::SUCCESSOR) == procDWhile);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(25, NodeType::PREDECESSOR) == procDWhile);
+
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(26, NodeType::SUCCESSOR) == procDWhile);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(26, NodeType::PREDECESSOR) == procDWhile);
+
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(27, NodeType::SUCCESSOR) == procDWhile);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(27, NodeType::PREDECESSOR) == procDWhile);
+
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(28, NodeType::SUCCESSOR) == procDWhile);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(28, NodeType::PREDECESSOR) == procDWhile);
+
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(29, NodeType::SUCCESSOR) == procDWhile);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(29, NodeType::PREDECESSOR) == procDWhile);
+
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(0, NodeType::SUCCESSOR) == EMPTY_RESULT);
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(0, NodeType::PREDECESSOR) == EMPTY_RESULT);
 
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(-1, NodeType::SUCCESSOR) == EMPTY_RESULT);
             Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(-1, NodeType::PREDECESSOR) == EMPTY_RESULT);
 
-            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(39, NodeType::SUCCESSOR) == EMPTY_RESULT);
-            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(39, NodeType::PREDECESSOR) == EMPTY_RESULT);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(41, NodeType::SUCCESSOR) == EMPTY_RESULT);
+            Assert::IsTrue(pkbNextStar.nextStarGetAllNodes(41, NodeType::PREDECESSOR) == EMPTY_RESULT);
         }
 
         TEST_METHOD(caching) {
