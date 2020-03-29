0a6b819dcbb8fd21252c33629da3b41b22803921
Aulud <44989315+Aulud@users.noreply.github.com>
2020-03-29 22:15:39 +0800

    Add stub class for Query Parser sub-component

---
+++ Team06/Code06/UnitTesting/QueryParserStub.cpp
@@ -0,0 +1,148 @@
+#include "stdafx.h"
+#include "QueryParserStub.h"
+#include "PQLTypes.h"
+
+using namespace PQL;
+
+using std::string;
+using std::vector;
+
+namespace UnitTesting {
+
+    QueryParserStub::QueryParserStub() : QueryParser() {
+        this->queryLog = { 0, { } };
+    }
+
+    Query QueryParserStub::parseQuery(string queryString) {
+        this->queryLog.first += 1;
+        this->queryLog.second.push_back(queryString);
+
+        if (queryString == StubQueries::EMPTY_QUERY) {
+            // Fails immediately during syntax validation - no fields populated
+            return {
+                SYNTAX_ERR_EMPTY_QUERY, queryString, false,
+                { },
+                { },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::BOOLEAN_VALID_QUERY) {
+            // Passes all validation
+            return {
+                STATUS_SUCCESS, queryString, true,
+                { },
+                { { "a", DesignEntity::ASSIGN } },
+                { },
+                {
+                    {
+                        "pattern a(_, _\"x\"_)", PatternType::ASSIGN_PATTERN, "a",
+                        { ArgType::WILDCARD, "_" },
+                        { ArgType::INCLUSIVE_PATTERN, "_x_" }
+                    }
+                },
+                { }
+            };
+        } else if (queryString == StubQueries::BOOLEAN_INVALID_DECL_SYNTAX_QUERY) {
+            // Fails immediately during syntax validation of declarations - no fields populated
+            return {
+                SYNTAX_ERR_INVALID_DECLARATION, queryString, false,
+                { },
+                { },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::BOOLEAN_UNKNOWN_DECL_KEYWORD_QUERY) {
+            // Fails during parsing of declarations - only terminates after parsing of return type
+            // No other fields populated except for the returnsBool boolean flag
+            return {
+                SYNTAX_ERR_UNKNOWN_DESIGN_ENTITY_KEYWORD, queryString, true,
+                { },
+                { },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::BOOLEAN_CONFLICTING_DECLARATION_QUERY) {
+            // Fails during insertion of second synonym mapping during parsing of declarations
+            // Only terminates after parsing of return type - returnsBool flag populated
+            return {
+                SEMANTIC_ERR_CONFLICTING_SYNONYM_DECLARATIONS, queryString, true,
+                { },
+                { { "r", DesignEntity::READ } },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::TUPLE_VALID_QUERY) {
+            // Passes all validation
+            return {
+                STATUS_SUCCESS, queryString, false,
+                { { "l1", AttrType::NONE }, { "l2", AttrType::NONE } },
+                { { "l1", DesignEntity::PROG_LINE }, { "l2", DesignEntity::PROG_LINE } },
+                {
+                    {
+                        "Next*(l1, l2)", RelationType::NEXTT,
+                        { ArgType::SYNONYM, "l1" },
+                        { ArgType::SYNONYM, "l2" },
+                        INVALID_ARG,
+                        INVALID_ARG
+                    }
+                },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::TUPLE_INVALID_DECL_SYNTAX_QUERY) {
+            // Fails immediately during syntax validation of declarations - no fields populated
+            return {
+                SYNTAX_ERR_INVALID_DECLARATION, queryString, false,
+                { },
+                { },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::TUPLE_UNKNOWN_DECL_KEYWORD_QUERY) {
+            // Fails during parsing of declarations - only terminates after parsing of return type
+            // targetEntities vector populated with tuple elements
+            return {
+                SYNTAX_ERR_UNKNOWN_DESIGN_ENTITY_KEYWORD, queryString, false,
+                { { "v1", AttrType::NONE }, { "v2", AttrType::NONE } },
+                { },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::TUPLE_CONFLICTING_DECLARATION_QUERY) {
+            // Fails during insertion of second synonym mapping during parsing of declarations
+            // Only terminates after parsing of return type - targetEntities populated
+            return {
+                SEMANTIC_ERR_CONFLICTING_SYNONYM_DECLARATIONS, queryString, false,
+                { { "s", AttrType::NONE }, { "c", AttrType::VALUE } },
+                { { "s", DesignEntity::STATEMENT }, { "c", DesignEntity::CONSTANT } },
+                { },
+                { },
+                { }
+            };
+        } else if (queryString == StubQueries::INVALID_RETURN_TYPE_QUERY) {
+            // Fails during parsing of return type - declarations populated
+            return {
+                STATUS_SUCCESS, queryString, false,
+                { },
+                { { "cl", DesignEntity::CALL } },
+                { },
+                { },
+                { }
+            };
+        } else {
+            // Query string is not in the list of predefined queries for this stubbed method
+            // Return an empty (dummy) Query object
+            Query query;
+            query.status = STATUS_SUCCESS;
+            query.queryString = queryString;
+            query.returnsBool = false;
+            return query;
+        }
+    }
+}
---
+++ Team06/Code06/UnitTesting/QueryParserStub.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "PQLTypes.h"
+#include "QueryUtils.h"
+#include "QueryParser.h"
+
+namespace UnitTesting {
+
+    namespace StubQueries {
+        const std::string EMPTY_QUERY = "";
+        const std::string BOOLEAN_VALID_QUERY = "assign a; Select BOOLEAN pattern a(_, _\"x\"_)";
+        const std::string BOOLEAN_INVALID_DECL_SYNTAX_QUERY = "procedure p & q; Select BOOLEAN";
+        const std::string BOOLEAN_UNKNOWN_DECL_KEYWORD_QUERY = "statement s; Select BOOLEAN";
+        const std::string BOOLEAN_CONFLICTING_DECLARATION_QUERY = "read r; print r; Select BOOLEAN";
+        const std::string TUPLE_VALID_QUERY = "prog_line l1, l2; Select <l1, l2> such that Follows*(l1, l2)";
+        const std::string TUPLE_INVALID_DECL_SYNTAX_QUERY = "variable v1 & v2; Select <v1, v2>";
+        const std::string TUPLE_UNKNOWN_DECL_KEYWORD_QUERY = "while w; progline l; Select <w, l>";
+        const std::string TUPLE_CONFLICTING_DECLARATION_QUERY = "stmt s; constant c; call c; Select <s, c.value>";
+        const std::string INVALID_RETURN_TYPE_QUERY = "call cl; Select [cl]";
+    }
+
+    /**
+     *  The QueryParserStub class is a stub for the Query Parser sub-component used
+     *  used by the Query Processor.
+     */
+    class QueryParserStub : public PQL::QueryParser {
+    public:
+        /**
+         *  Empty constructor for the QueryParser stub.
+         */
+        QueryParserStub();
+
+        /**
+         *  Stubbed method for the Query Parser sub-component. Returns pre-constructed
+         *  Query objects for a specific set of queries, or otherwise an empty Query
+         *  struct with its default values.
+         */
+        PQL::Query parseQuery(std::string queryString);
+    private:
+        std::pair<int, std::vector<std::string>> queryLog;
+    };
+
+}
