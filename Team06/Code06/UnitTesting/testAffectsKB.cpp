c2cb6a0d8b7d9b2d6847b79380a84397f442be7c
laichengyu <laichengyu95@gmail.com>
2020-03-30 23:08:44 +0800

    Update AffectsKB unit tests

--- Team06/Code06/UnitTesting/testAffectsKB.cpp
+++ Team06/Code06/UnitTesting/testAffectsKB.cpp
@@ -28,6 +28,18 @@ namespace UnitTesting {
             aKB.affectsSetProcessedDirect(3, NodeType::PREDECESSOR);
             aKB.affectsSetProcessedDirect(2, NodeType::PREDECESSOR);
             aKB.affectsSetProcessedDirect(1, NodeType::PREDECESSOR);
+
+            aKB.addAffectsStar(1, 2);
+            aKB.addAffectsStar(1, 3);
+            aKB.addAffectsStar(2, 3);
+            aKB.affectsStarSetProcessedAll(1, NodeType::SUCCESSOR);
+            aKB.affectsStarSetProcessedAll(2, NodeType::SUCCESSOR);
+            aKB.affectsStarSetProcessedAll(3, NodeType::SUCCESSOR);
+            aKB.affectsStarSetProcessedAll(3, NodeType::PREDECESSOR);
+            aKB.affectsStarSetProcessedAll(2, NodeType::PREDECESSOR);
+            aKB.affectsStarSetProcessedAll(1, NodeType::PREDECESSOR);
+
+            aKB.setAffectsFullyComputed();
         }
 
         TEST_METHOD(addAffects) {
@@ -38,6 +50,16 @@ namespace UnitTesting {
             Assert::IsTrue(aKB.affectsGetDirectNodes(1, NodeType::PREDECESSOR) == emptyResult);
             Assert::IsTrue(aKB.affectsGetDirectNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
             Assert::IsTrue(aKB.affectsGetDirectNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{2});
+        }
+
+        TEST_METHOD(addAffectsStar) {
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(1, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{2, 3});
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(2, NodeType::SUCCESSOR) == std::unordered_set<StmtId>{3});
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(3, NodeType::SUCCESSOR) == emptyResult);
+
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(1, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(2, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1});
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(3, NodeType::PREDECESSOR) == std::unordered_set<StmtId>{1, 2});
         }
 
         TEST_METHOD(affects) {
@@ -54,6 +76,20 @@ namespace UnitTesting {
             Assert::IsFalse(aKB.affects(5, -1));
         }
 
+        TEST_METHOD(affectsStar) {
+            Assert::IsTrue(aKB.affectsStar(1, 2));
+            Assert::IsTrue(aKB.affectsStar(2, 3));
+            Assert::IsTrue(aKB.affectsStar(1, 3));
+
+            Assert::IsFalse(aKB.affectsStar(2, 1));
+            Assert::IsFalse(aKB.affectsStar(3, 1));
+            Assert::IsFalse(aKB.affectsStar(3, 2));
+
+            Assert::IsFalse(aKB.affectsStar(0, 1));
+            Assert::IsFalse(aKB.affectsStar(1, -1));
+            Assert::IsFalse(aKB.affectsStar(5, -1));
+        }
+
         TEST_METHOD(affectsGetDirectNodes) {
             Assert::IsTrue(aKB.affectsGetDirectNodes(0, NodeType::SUCCESSOR) == emptyResult);
             Assert::IsTrue(aKB.affectsGetDirectNodes(5, NodeType::SUCCESSOR) == emptyResult);
@@ -62,6 +98,16 @@ namespace UnitTesting {
             Assert::IsTrue(aKB.affectsGetDirectNodes(0, NodeType::PREDECESSOR) == emptyResult);
             Assert::IsTrue(aKB.affectsGetDirectNodes(5, NodeType::PREDECESSOR) == emptyResult);
             Assert::IsTrue(aKB.affectsGetDirectNodes(-1, NodeType::PREDECESSOR) == emptyResult);
+        }
+
+        TEST_METHOD(affectsStarGetAllNodes) {
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(0, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(5, NodeType::SUCCESSOR) == emptyResult);
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(-1, NodeType::SUCCESSOR) == emptyResult);
+
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(0, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(5, NodeType::PREDECESSOR) == emptyResult);
+            Assert::IsTrue(aKB.affectsStarGetAllNodes(-1, NodeType::PREDECESSOR) == emptyResult);
         }
 
         TEST_METHOD(affectsSetProcessedDirect) {
@@ -75,6 +121,23 @@ namespace UnitTesting {
 
             Assert::IsFalse(aKB.affectsProcessedDirect(4, NodeType::SUCCESSOR));
             Assert::IsFalse(aKB.affectsProcessedDirect(4, NodeType::PREDECESSOR));
+        }
+
+        TEST_METHOD(affectsStarSetProcessedAll) {
+            Assert::IsTrue(aKB.affectsStarProcessedAll(1, NodeType::SUCCESSOR));
+            Assert::IsTrue(aKB.affectsStarProcessedAll(2, NodeType::SUCCESSOR));
+            Assert::IsTrue(aKB.affectsStarProcessedAll(3, NodeType::SUCCESSOR));
+
+            Assert::IsTrue(aKB.affectsStarProcessedAll(1, NodeType::PREDECESSOR));
+            Assert::IsTrue(aKB.affectsStarProcessedAll(2, NodeType::PREDECESSOR));
+            Assert::IsTrue(aKB.affectsStarProcessedAll(3, NodeType::PREDECESSOR));
+
+            Assert::IsFalse(aKB.affectsStarProcessedAll(4, NodeType::SUCCESSOR));
+            Assert::IsFalse(aKB.affectsStarProcessedAll(4, NodeType::PREDECESSOR));
+        }
+
+        TEST_METHOD(setAffectsFullyComputed) {
+            Assert::IsTrue(aKB.allAffectsFullyComputed());
         }
     };
 }
