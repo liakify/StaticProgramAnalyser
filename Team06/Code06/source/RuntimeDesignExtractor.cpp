683101270a0d8e7394a418607db9959a6e9019ad
laichengyu <laichengyu95@gmail.com>
2020-04-05 00:58:45 +0800

    Refactor nextStarDFS

--- Team06/Code06/source/RuntimeDesignExtractor.cpp
+++ Team06/Code06/source/RuntimeDesignExtractor.cpp
@@ -90,17 +90,17 @@ namespace FrontEnd {
     void RuntimeDesignExtractor::nextStarDFS(StmtId root, StmtId curr, std::unordered_set<StmtId>& visited, NodeType type) {
         visited.insert(curr);
 
-        if (pkb->nextStarProcessedAll(curr, type)) {  // Query cache
-            for (StmtId id : pkb->nextStarGetAllNodes(curr, type)) {
-                visited.insert(id);
-                addBiDirEdge(root, id, type);
-            }
-            return;
-        }
-
         std::unordered_set<StmtId> neighbours = pkb->nextStarGetDirectNodes(curr, type);
 
         for (const auto& n : neighbours) {
+            if (pkb->nextStarProcessedAll(n, type)) {  // Query cache
+                for (StmtId id : pkb->nextStarGetAllNodes(n, type)) {
+                    visited.insert(id);
+                    addBiDirEdge(root, id, type);
+                }
+                continue;
+            }
+
             // Add bi-directional edge first before cycle check for Next*(s, s)
             addBiDirEdge(root, n, type);
             if (visited.find(n) == visited.end()) {
