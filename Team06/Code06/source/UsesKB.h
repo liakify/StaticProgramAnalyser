f6826019c7489dd08877d82497e0d468167ba234
laichengyu <laichengyu95@gmail.com>
2020-04-09 23:50:20 +0800

    Fix linting

--- Team06/Code06/source/UsesKB.h
+++ Team06/Code06/source/UsesKB.h
@@ -10,7 +10,7 @@
 namespace PKB {
 
     class UsesKB {
-    public:
+     public:
         void addStmtUses(StmtId stmtId, VarId varId);
 
         bool stmtUses(StmtId stmtId, VarId varId);
@@ -27,7 +27,7 @@ namespace PKB {
 
         const std::unordered_set<ProcId>& getAllProcUsingVar(VarId varId);
 
-    private:
+     private:
         std::unordered_map<StmtId, std::unordered_set<VarId>> stmtVarTable;
         std::unordered_map<VarId, std::unordered_set<StmtId>> varStmtTable;
         std::unordered_map<ProcId, std::unordered_set<VarId>> procVarTable;
