177e5e5a377f0f80fb987f9dea2c5b6f681dcea0
laichengyu <laichengyu95@gmail.com>
2020-04-10 21:24:15 +0800

    Add comment to explain design decision of public PKB member classes

--- Team06/Code06/source/PKB.h
+++ Team06/Code06/source/PKB.h
@@ -166,6 +166,27 @@ namespace PKB {
         */
         void clear();
 
+        /*
+            Following the DRY principle, the PKB layer does not expose the public APIs of the below public tables/KBs.
+
+            Note that the internal data structures of these classes are still private,
+            and thus cannot be modified directly via C++ library and require usage of the class' public API.
+            Hence, coupling between PKB-PQL and FrontEnd-PKB is already minimised
+            as any changes to internal implementations of these classes 
+            are very unlikely to significantly impact either FrontEnd or PQL.
+
+            Post DesignExtractor, these tables/KBs are also guaranteed to not be modified.
+            Thus, the structure of PKB (w.r.t the below public tables) is guaranteed to not change. 
+            Hence they do not necessarily need to be hidden and is safe for access.
+
+            Additionally, adding boilerplate to the PKB layer that essentially only calls APIs of the below tables/KBs
+            requires significant amounts of time to implement tedious refactoring, 
+            introduces more overhead taking up precious evaluation time away from the Query Evaluator
+            while serving no additional benefit.
+
+            For NextKB and AffectsKB, due to involvement of the RuntimeDesignExtractor,
+            PKB layer API is used to avoid multiple cyclic dependencies.
+        */
         VarTable varTable;
         ProcTable procTable;
         ConstTable constTable;
